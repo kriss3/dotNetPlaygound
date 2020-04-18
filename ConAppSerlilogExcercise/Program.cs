6d4bc534d9c92b01a451825a9636896c604d4988
Kris <kris_s2@outlook.com>
2020-04-18 11:26:54 -0700

    Adding Serlilog project to solution

---
+++ ConAppSerlilogExcercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConAppSerlilogExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
