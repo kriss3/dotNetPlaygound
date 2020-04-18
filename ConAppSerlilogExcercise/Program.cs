481b5e3bb93d8e7dce6323e0de3762b82c41caa9
Kris <kris_s2@outlook.com>
2020-04-18 11:50:10 -0700

    Adding serilog library and sinks for mssql, file, console

--- ConAppSerlilogExcercise/Program.cs
+++ ConAppSerlilogExcercise/Program.cs
@@ -6,7 +6,10 @@ namespace ConAppSerlilogExcercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Start !");
+
+
+
         }
     }
 }
