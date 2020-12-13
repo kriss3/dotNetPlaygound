fbce2b3de3254add71f38ea08459ccbc864ecfb4
Kris <kris_s2@outlook.com>
2020-12-13 15:16:43 -0800

    KS-Adding simple project; updating to dotNet framework 4.8 (was curious why default was 4.7.2); Adding first excercise from HackerRank; Pushing all to newly created develop branch of the existing repo.

---
+++ ConAppPbP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+namespace ConAppPbP
+{
+    class Program
+    {
+        static void Main()
+        {
+            WriteLine("===============");
+            WriteLine("===== START =====");
+
+            InputValues();
+
+            ReadLine();
+        }
+
+        private static void InputValues() 
+        {
+            string s = ReadLine();
+            string d = ReadLine();
+            string i = ReadLine();
+
+
+            WriteLine($"String is: {s.Trim()}");
+            WriteLine($"Double is: {d:2}");
+            WriteLine($"Int is: {i}");
+        }
+    }
+}
