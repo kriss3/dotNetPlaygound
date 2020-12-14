33bd1f8b2216a674d810d305bf364da335dc1bd1
Kris <kris_s2@outlook.com>
2020-12-14 00:33:14 -0800

    KS-playing with built in Array functions;

--- ConAppPbP/Driver.cs
+++ ConAppPbP/Driver.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using static System.Console;
 namespace ConAppPbP
 {
-    class Program
+    class Driver
     {
         static void Main()
         {
@@ -15,7 +15,9 @@ namespace ConAppPbP
             WriteLine("===== START =====");
 
             //InputValues();
-            ArrayWork();
+            //ArrayWork();
+
+            SortAndReverseArray();
 
             ReadLine();
         }
@@ -48,6 +50,26 @@ namespace ConAppPbP
         private static void PrintStar(int num) {
             for (int i = 0; i < num; i++) {
                 Write("*");
+            }
+        }
+
+        public static void SortAndReverseArray() 
+        {
+            int[] arr = {7,4,6,3,5,8,11,2,15,1};
+
+            foreach (var item in arr)
+            {
+                Write($"{item} ");
+            }
+
+            WriteLine();
+            WriteLine("==== Sort and Reverse ====\n");
+            Array.Sort(arr);
+            Array.Reverse(arr);
+
+            foreach (var item in arr)
+            {
+                Write($"{item} ");
             }
         }
     }
