448c917b3324dd7f5af4e8922af3f89ef0c5cb53
Kris <kris_s2@outlook.com>
2020-12-20 23:53:14 -0800

    KS-Adding simple methods to Calculator class for tests; Adding unit tests for the first method as example;

---
+++ TestingUnitTests/CalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+namespace TestingUnitTests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_AddsCorrectlyTwoNumber_ReturnsNumber() 
+        {
+            //Arrange
+            var actual = Calculator.Add(10, 10);
+            //Act
+            var expected = 20;
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
