029be134f6788f58bf8d06c7a0a6e8fbc7a7098a
Kris <kris_s2@outlook.com>
2020-12-20 22:01:35 -0800

    KS-Adding new project for playground with xUnit/TDD;

---
+++ TestingUnitTests/Reservation.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Win32;using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingUnitTests
+{
+    public class Reservation
+    {
+        public User MadeBy { get; set; }
+
+        public bool CanBeCanceledBy(User user) 
+        {
+            bool result;
+            if (user.isAdmin)
+            {
+                result = true;
+            }
+
+            if (MadeBy == user) { }
+            {
+                result = true;
+            }
+            return result;
+        }
+    }
+}
