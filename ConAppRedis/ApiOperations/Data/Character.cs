d2680441835187468a8763411ed7c196269ca8ac
Kris <kris_s2@outlook.com>
2022-08-11 23:11:45 -0700

    Latest changes - Extension Method and Redis

---
+++ ConAppRedis/ApiOperations/Data/Character.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ConAppRedis.ApiOperations.Data
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Occupation { get; set; }
+        public string? Gender { get; set; }
+        public string? PictureUrl { get; set; }
+        public int Votes { get; set; }
+    }
+}
---
+++ ConAppRedis/Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Configuration;
+using static System.Console;
+
+using ConAppRedis.Extensions;
+using ConAppRedis.ApiOperations.Data;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Caching.Distributed;
+
+WriteLine("Hello, World!");
+WriteLine("Getting Configurtion");
+
+ConfigurationBuilder configuration = new();
+configuration.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json");
+
+var config = configuration.Build();
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+    {
+        services.AddSingleton<CharacterService>();
+        services.AddStackExchangeRedisCache(opt => {
+            opt.InstanceName = "ConAppRedis_";
+            opt.Configuration = config.GetConnectionString("Redis");
+        });
+    })
+    .Build();
+
+await LoadData(host.Services);
+
+await host.RunAsync();
+
+WriteLine($"Redis connectionString is: {config.GetConnectionString("Redis")}");
+WriteLine("Load Data...");
+
+ReadLine();
+
+static async Task LoadData(IServiceProvider services) 
+{
+    using IServiceScope serviceScope = services.CreateScope();
+    IServiceProvider provider = serviceScope.ServiceProvider;
+
+    IEnumerable<Character>? data;
+    //Get Array of Peope from API
+
+    string recordKey = $"DataKey_{DateTime.Now.ToString("yyyyMMdd_hhmm")}";
+
+    IDistributedCache cache = provider.GetRequiredService<IDistributedCache>();
+    CharacterService svc = provider.GetRequiredService<CharacterService>();
+
+
+    data = await cache.GetRecordAsync<Character[]>(recordKey);
+
+    if (data is null)
+    {
+        WriteLine($"Getting data from from the service.");
+        data = await svc.GetCharacterAsync();
+        await cache.SetRecordAsync(recordKey, data);
+    }
+    else { 
+        //get data from Cache
+    }
+
+}
