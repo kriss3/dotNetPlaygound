a9d3c10303e7fb84fd49cb1155fba4a861c441e9
Kris <kris_s2@outlook.com>
2022-08-16 00:15:51 -0700

    Latest changes excercising usage of records, and preparation for usage of discriminated union using abstract class and enums

---
+++ ConAppDu/DriverOrApi/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace ConAppDu.DriverOrApi;
+
+using System.Diagnostics;
+using static System.Console;
+
+internal class Program
+{
+    static void Main()
+    {
+        WriteLine("Hello from the Driver...");
+        Helper hp = new("BioTrack");
+
+        if (hp.MyList.All(s => !string.IsNullOrWhiteSpace(s)))
+        {
+            WriteLine("All emements are valid.");
+        }
+        else
+        {
+            WriteLine("Some elements are empty or null.");
+        }
+
+        Parent c1 = new Child1();
+        Parent c2 = new Child2();
+
+
+        Debug.Print($"{c1.Type}");
+        Debug.Print($"{c2.Type}");
+
+        ReadLine();
+    }
+}
+
+public class Helper
+{
+    private readonly string _control;
+
+    public List<string> MyList { get; set; } = new List<string>() { "test1", "test2", "test3", "test4" };
+
+    public Helper(string myControl)
+    {
+        _control = myControl;
+    }
+}
+
+public abstract record Parent(Enums.TraceType Type);
+
+public record Child1() : Parent(Enums.TraceType.BioTrack);
+
+public record Child2() : Parent(Enums.TraceType.Metrc);
+
+public class Enums
+{
+    public enum TraceType
+    {
+        BioTrack,
+        Metrc
+    }
+}
+
+
+
+
