0886d0d3c20059f176d9111af9bff11a5365a7a9
Kris <kris_s2@outlook.com>
2024-04-11 22:10:02 -0700

    kata: Adding a project to deal with Lambda expresions

---
+++ ConAppPlayingWithLambda/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConAppPlayingWithLambda;
+
+public class Program
+{
+	static async Task Main() => await Task.Run(() => Console.WriteLine("Lambda Expressions"));
+}
+
+public class MxCurrency 
+{
+	public string? Code { get; set; }
+    public string? Description { get; set; }
+}
+
+public class UdfCurrency 
+{
+    public string? Code { get; set; }
+    public string? Description { get; set; }
+}
+
+
