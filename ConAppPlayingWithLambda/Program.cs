9d88791c52b5ca5928034f7d9ee91cc4d46bec16
Kris <kris_s2@outlook.com>
2024-04-13 15:13:21 -0700

    kata: preparing data for business logic application experiment. Loading data from two different sources and populating descirpiton if non is available;

--- ConAppPlayingWithLambda/Program.cs
+++ ConAppPlayingWithLambda/Program.cs
@@ -8,23 +8,23 @@ public class Program
 	{
 		return
 		[
-			new UdfCurrency { Code = "USD", Description = "United States Dollar" },
-		new UdfCurrency { Code = "EUR", Description = "Euro" },
-		new UdfCurrency { Code = "JPY", Description = "Japanese Yen" },
-		new UdfCurrency { Code = "GBP", Description = "British Pound" },
-		new UdfCurrency { Code = "AUD", Description = "Australian Dollar" },
-		new UdfCurrency { Code = "CAD", Description = "Canadian Dollar" },
-		new UdfCurrency { Code = "CHF", Description = "Swiss Franc" },
-		new UdfCurrency { Code = "CNY", Description = "Chinese Yuan" },
-		new UdfCurrency { Code = "SEK", Description = "Swedish Krona" },
-		new UdfCurrency { Code = "NZD", Description = "New Zealand Dollar" }
+			new UdfCurrency { Code = "USD", Description = "" },
+			new UdfCurrency { Code = "EUR", Description = "" },
+			new UdfCurrency { Code = "JPY", Description = "" },
+			new UdfCurrency { Code = "GBP", Description = "" },
+			new UdfCurrency { Code = "AUD", Description = "" },
+			new UdfCurrency { Code = "CAD", Description = "" },
+			new UdfCurrency { Code = "CHF", Description = "" },
+			new UdfCurrency { Code = "CNY", Description = "" },
+			new UdfCurrency { Code = "SEK", Description = "" },
+			new UdfCurrency { Code = "NZD", Description = "" }
 		];
 	}
 
 	public static List<MxCurrency> GetMxCurrencies()
 	{
-		return new List<MxCurrency>
-	{
+		return
+			[
 		new MxCurrency { Code = "MXN", Description = "Mexican Peso" },
 		new MxCurrency { Code = "USD", Description = "United States Dollar" },
 		new MxCurrency { Code = "EUR", Description = "Euro" },
@@ -35,10 +35,8 @@ public class Program
 		new MxCurrency { Code = "CHF", Description = "Swiss Franc" },
 		new MxCurrency { Code = "CNY", Description = "Chinese Yuan" },
 		new MxCurrency { Code = "SEK", Description = "Swedish Krona" }
-	};
+		];
 	}
-
-
 }
 
 public class MxCurrency 
