38a6759c8d224ab1bc440e3993dc90055508a940
Kris <kris_s2@outlook.com>
2024-05-08 23:28:48 -0700

    kata: switching to use Serilog instead of the default logger;

--- WebsiteStatus/Program.cs
+++ WebsiteStatus/Program.cs
@@ -1,13 +1,34 @@
+using Serilog;
+using Serilog.Events;
+
 namespace WebsiteStatus;
 
 public class Program
 {
 	public static void Main(string[] args)
 	{
-		var builder = Host.CreateApplicationBuilder(args);
-		builder.Services.AddHostedService<Worker>();
 
-		var host = builder.Build();
-		host.Run();
+		Log.Logger = new LoggerConfiguration()
+			.MinimumLevel.Debug()
+			.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+			.Enrich.FromLogContext()
+			.WriteTo.File(@"C:\temp\WebsiteStatus\HealthCheck.txt")
+			.CreateLogger();
+
+		try
+		{
+			Log.Information("Starting up the service");
+
+			var builder = Host.CreateApplicationBuilder(args);
+			builder.Services.AddHostedService<Worker>();
+
+			var host = builder.Build();
+			host.Run();
+		}
+		catch (Exception ex)
+		{
+			Log.Fatal($"There was a problem starting the service. Error:{ex.Message}");
+			return;
+		}
 	}
 }
