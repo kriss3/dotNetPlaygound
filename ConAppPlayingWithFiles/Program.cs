cd9cdafdf6d4488f2c68bd3ab2f3d68944eddec6
Kris <kris_s2@outlook.com>
2024-05-23 21:00:53 -0700

    kata: Working with FileSystem. Refreshing info library about Files/Directories/Streams

---
+++ ConAppPlayingWithFiles/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConAppPlayingWithFiles;
+
+public class Program
+{
+	static Task Main()
+	{
+		Console.WriteLine("Hello, World!");
+		return Task.CompletedTask;
+	}
+
+	private async Task Run() 
+	{
+		string rootPath = @"C:\temp\TimCorey_Files";
+	}
+}
