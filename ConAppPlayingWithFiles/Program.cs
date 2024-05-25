fcd3b2971522aa2c9a73e175b0185cf568b6c20b
Kris <kris_s2@outlook.com>
2024-05-24 23:06:36 -0700

    kata: playing with files and file names across multiple dirs;

--- ConAppPlayingWithFiles/Program.cs
+++ ConAppPlayingWithFiles/Program.cs
@@ -1,6 +1,5 @@
 ﻿
 using static System.Console;
-using System.IO;
 
 namespace ConAppPlayingWithFiles;
 
@@ -12,12 +11,58 @@ public class Program
 		await Run();
 	}
 
-	private static Task Run() 
+	private static Task Run()
+	{
+		//SearchDirAtRootLevelOnly();
+		//SearchDirAtAllLevels();
+		SearchAllFilesAtRoot();
+
+		return Task.CompletedTask;
+	}
+
+	private static Task SearchDirAtAllLevels()
+	{
+		string rootPath = @"C:\temp\TimCorey_Files";
+		string[] dirs = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+
+		dirs.ToList().ForEach(d => WriteLine($"Directory Name: {d}"));
+
+		return Task.CompletedTask;
+	}
+
+	private static Task SearchDirAtRootLevelOnly()
 	{
 		string rootPath = @"C:\temp\TimCorey_Files";
 		string[] dirs = Directory.GetDirectories(rootPath);
 
 		dirs.ToList().ForEach(d => WriteLine($"Directory Name: {d}"));
 		return Task.CompletedTask;
-    }
+	}
+
+	private static Task SearchAllFilesAtRoot()
+	{
+		string rootPath = @"C:\temp\TimCorey_Files";
+		var files = Directory.GetFiles(rootPath, "*.*", SearchOption.TopDirectoryOnly);
+
+		files.ToList().ForEach(f => WriteLine($"File names are: {f}"));
+
+		return Task.CompletedTask;
+	}
+
+	private static Task SearchAllFiles()
+	{
+		string rootPath = @"C:\temp\TimCorey_Files";
+		var files = Directory.GetFiles(rootPath, "*.*", SearchOption.AllDirectories);
+
+		files.ToList().ForEach(f => WriteLine($"File names are: {f}"));
+
+		files.ToList().ForEach(f => WriteLine($"File names are: {Path.GetDirectoryName(f)}"));
+
+		files.ToList().ForEach(f => WriteLine($"File names are: {Path.GetFileNameWithoutExtension(f)})"));
+
+		files.ToList().ForEach(f => WriteLine($"File names are: {Path.GetFullPath(f)}"));
+
+
+		return Task.CompletedTask;
+	}
 }
