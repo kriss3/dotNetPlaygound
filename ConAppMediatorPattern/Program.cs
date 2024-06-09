b80b3370411b6396bfac5c56a52c1159d3947d69
Kris <kris_s2@outlook.com>
2024-06-08 23:59:21 -0700

    kata: Adding Mediator pattern proj. to follow the PS course

---
+++ ConAppMediatorPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace ConAppMediatorPattern;
+
+public class Program
+{
+	static void Main(string[] args)
+	{
+		WriteLine("Project to learn more about this Behavioral Pattern.");
+		WriteLine("Based on PluralSight Course by: Steve Michelotti");
+	}
+}
