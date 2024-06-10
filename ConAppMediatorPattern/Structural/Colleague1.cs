309fc79ab57d173659f9e985f4d5a176c8968b89
Kris <kris_s2@outlook.com>
2024-06-09 23:57:27 -0700

    kata: adding implementation of the concrete Colleague named Colleage1;

---
+++ ConAppMediatorPattern/Structural/Colleague1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+namespace ConAppMediatorPattern.Structural;
+public class Colleague1 : Colleague
+{
+    public Colleague1(Mediator mediator): base(mediator)
+    {
+            
+    }
+    public override void HandleNotification(string message)
+	{
+        WriteLine($"Colleague1 receives notification message: {message}");
+	}
+}
