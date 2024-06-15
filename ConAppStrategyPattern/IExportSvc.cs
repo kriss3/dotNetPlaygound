56065e05d1ea82cd878b42c6f0602e7986da1963
Kris <kris_s2@outlook.com>
2024-06-15 09:59:51 -0700

    kata: providing 3 concrete strategy based on a common interface. Client is able to choose which 'strategy'policy to use.

--- ConAppStrategyPattern/IExportSvc.cs
+++ ConAppStrategyPattern/IExportSvc.cs
@@ -10,4 +10,32 @@ public interface IExportSvc
 	void Export(Order order);
 }
 
-public record Order();
+/// <summary>
+/// Concrete Strategy here...
+/// </summary>
+public class XmlExportService : IExportSvc 
+{
+	public void Export(Order order) 
+	{
+		Console.WriteLine($"Exporting {order.Name} to XML.");
+	}
+}
+
+public class JsonExportService : IExportSvc
+{
+	public void Export(Order order) 
+	{
+		Console.WriteLine($"Exporting {order.Name} to JSON");
+	}
+}
+
+public class ExcelExportService : IExportSvc
+{
+	public void Export(Order order) 
+	{
+		Console.WriteLine($"Exporting {order.Name} to MS Excel");
+	}
+}
+
+
+public record Order(string Name);
