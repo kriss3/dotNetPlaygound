22522065da1ec8456715050b738e4795722260a3
Kris <kris_s2@outlook.com>
2024-06-15 09:48:01 -0700

    kata: adding description of work for this pattern.

--- ConAppStrategyPattern/Program.cs
+++ ConAppStrategyPattern/Program.cs
@@ -6,6 +6,10 @@ public class Program
 {
 	static async Task Main(string[] args)
 	{
-		await Task.Run(() => WriteLine("Hello, World!")); 
+		await Task.Run(() => 
+		WriteLine("Description: " +
+		"Strategy Patterns, one of the behavior patterns aka Policy pattern." +
+		"Client decides during the run time which 'algorithm' to execute." +
+		"Example will use Export (csv, excel, json etc) but other use cases could be Payment methods.")); 
 	}
 }
