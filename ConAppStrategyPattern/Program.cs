695ddb2a3aee491c4a3044e875d235fdfbfaa1fd
Kris <kris_s2@outlook.com>
2024-06-15 09:43:34 -0700

    kata: Reviewing Strategy pattern based on PS course by Kevin Dockx

---
+++ ConAppStrategyPattern/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using static System.Console;
+namespace ConAppStrategyPattern;
+
+public class Program
+{
+	static async Task Main(string[] args)
+	{
+		await Task.Run(() => WriteLine("Hello, World!")); 
+	}
+}
