471bf654bd7dfc2c2ff951940ecb2ab51af2f21c
Kris <kris_s2@outlook.com>
2024-07-03 23:21:17 -0700

    kata: implementing ConcreteCommand and Invoke class;

--- ConAppCommandPattern/Program.cs
+++ ConAppCommandPattern/Program.cs
@@ -5,7 +5,7 @@ namespace ConAppCommandPattern;
 
 public class Program
 {
-    static async Task Main(string[] args)
+    static async Task Main()
     {
         WriteLine("Hello, World!");
         await Run();
@@ -17,9 +17,9 @@ public class Program
         {
             //this is my driver function. 
             //I need: Receiver, Command, Invoker
-            Receiver rc = new Receiver();
-            Command cmd = new Command(rc);
-            Inoker inv = new Inoker();  
+            Receiver rc = new();
+            Command cmd = new ConcreteCommand(rc);
+            Invoker inv = new();
 
             // now that I have infra ready make a setup:
 
@@ -33,7 +33,7 @@ public class Program
 
 public class Receiver
 {
-    public void Action()
+    public static void Action()
     {
         WriteLine("Receiver.Action() called");
     }
@@ -44,4 +44,26 @@ public abstract class Command(Receiver receiver)
     protected Receiver _receiver = receiver;
 
     public abstract void Execute();
+}
+
+public class  ConcreteCommand(Receiver receiver) : Command(receiver ?? new Receiver()) 
+{
+    public override void Execute()
+    {
+        Receiver.Action();
+    }
+}
+
+public class Invoker 
+{
+    Command? _cmd;
+
+    public void SetCommand(Command cmd) 
+    {
+        _cmd = cmd;
+    }
+    public void ExecuteCommand()
+    {
+        _cmd?.Execute();
+    }
 }
