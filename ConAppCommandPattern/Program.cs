ce3b6f2957b872d341ae30d21b873e98da188483
Kris <kris_s2@outlook.com>
2024-07-03 22:49:58 -0700

    kata: working with Command Pattern. Adding infra for this project.

---
+++ ConAppCommandPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+using static System.Console;
+
+namespace ConAppCommandPattern;
+
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        WriteLine("Hello, World!");
+        await Run();
+    }
+
+    private static async Task Run() 
+    {
+        await Task.Run(() =>
+        {
+
+
+        });
+    }
+}
