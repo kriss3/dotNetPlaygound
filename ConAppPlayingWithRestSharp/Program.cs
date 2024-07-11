724f5dfdd7e520778cb6c1a26b47d4d04fbaaee7
Kris <kris_s2@outlook.com>
2024-07-10 23:56:45 -0700

    kata: working on builder in DI for conn application

--- ConAppPlayingWithRestSharp/Program.cs
+++ ConAppPlayingWithRestSharp/Program.cs
@@ -1,4 +1,9 @@
 ﻿
+using ConAppPlayingWithRestSharp.Service;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RestSharp;
+using System.Runtime.CompilerServices;
 using static System.Console;
 
 namespace ConAppPlayingWithRestSharp;
@@ -7,6 +12,39 @@ public class Program
 {
 	static async Task Main(string[] args)
 	{
-		await Task.Run(() => { WriteLine("Playing with RestSharp Lib"); });  
+		await Task.Run(() => 
+		{ 
+			WriteLine("Playing with RestSharp Lib");
+            using var host = CreateHostBuilder().Build();
+            var exampleUsage = host.Services.GetRequiredService<ExampleUsage>();
+
+            try
+			{
+                var data = await _apiService.GetAsync<MyDataModel>("endpoint");
+                if (data != null)
+                {
+                    WriteLine("Request succeeded");
+                }
+                else
+                {
+                    Console.WriteLine("No data returned");
+                }
+            }
+			catch (Exception ex)
+			{
+                WriteLine($"An error occurred: {ex.Message}");
+			}
+		});  
 	}
+
+    private static IHostBuilder CreateHostBuilder() =>
+        Host.CreateDefaultBuilder()
+            .ConfigureServices((_, services) =>
+            {
+                services.AddSingleton<IApiService, ApiService>();
+                services.AddSingleton<RestClient>();
+                services.AddSingleton<ExampleUsage>();
+            });
+
+
 }
