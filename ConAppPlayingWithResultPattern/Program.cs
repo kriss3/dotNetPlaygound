42670a23dda23b62d93f83adc76d7bc38f2e005a
Kris <kris_s2@outlook.com>
2024-07-21 22:58:53 -0700

    kata: Adding skeleton proj for playing wiht Optional and Result type.

---
+++ ConAppPlayingWithResultPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConAppPlayingWithResultPattern;
+
+public class Program
+{
+	static async Task Main()
+	{
+		await Task.Run(Console.WriteLine);
+	}
+
+
+}
+
+public class Result
+{
+	public bool IsSuccess { get; set; }
+	public string? Message { get; set; }
+
+	private Result(bool isSuccess, string? message)
+	{
+		IsSuccess = isSuccess;
+		Message = message;
+	}
+
+	public static Result Success() => new(true, null);
+	public static Result Failed(string message) => new(false, message);
+}
