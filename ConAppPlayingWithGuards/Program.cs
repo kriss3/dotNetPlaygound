5032a2e2e26fdcebd786d2c46bd449070ce4f02b
Kris <kris_s2@outlook.com>
2024-08-19 23:22:31 -0700

    kata: preparing new project to play with guards. Custom and with exteral Ardalis library;

---
+++ ConAppPlayingWithGuards/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConAppPlayingWithGuards;
+
+using static System.Console;
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        WriteLine("Hello, World!");
+        await Task.CompletedTask;
+    }
+}
