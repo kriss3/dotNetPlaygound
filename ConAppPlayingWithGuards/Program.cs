39407c3ffce857c317703408cbe5ff147df14f72
Kris <kris_s2@outlook.com>
2024-08-19 23:23:31 -0700

    kata: adding a new custom guard;

--- ConAppPlayingWithGuards/Program.cs
+++ ConAppPlayingWithGuards/Program.cs
@@ -6,6 +6,16 @@ public class Program
     static async Task Main(string[] args)
     {
         WriteLine("Hello, World!");
+        TestGuard();
         await Task.CompletedTask;
+    }
+
+    private static void TestGuard()
+    {
+        var age = 17;
+        if (age < 18)
+        {
+            throw new Exception("You are not allowed to enter this site.");
+        }
     }
 }
