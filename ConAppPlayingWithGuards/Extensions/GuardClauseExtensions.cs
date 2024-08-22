502600c8792f22ac6e18406689a5b0e71ea53f31
Kris <kris_s2@outlook.com>
2024-08-21 23:41:53 -0700

    kata: Order cleanup and adding usage of the newly created guard;

--- ConAppPlayingWithGuards/Extensions/GuardClauseExtensions.cs
+++ ConAppPlayingWithGuards/Extensions/GuardClauseExtensions.cs
@@ -11,4 +11,26 @@ public static class GuardClauseExtensions
             throw new ArgumentOutOfRangeException(parameterName, "Age must be between 1 and 120.");
         }
     }
+
+    public static void AgainstInvalidOrder(this IGuardClause guardClause, Order order, string parameterName)
+    {
+        Guard.Against.Null(order, parameterName);
+        Guard.Against.InTheFuture(order.OrderDate, nameof(order.OrderDate));
+        Guard.Against.InvalidCustomer(order.Customer, nameof(order.Customer));
+        Guard.Against.Empty(order.OrderItems, nameof(order.OrderItems));
+
+        foreach (var item in order.OrderItems)
+        {
+            Guard.Against.InvalidOrderItem(item, nameof(order.OrderItems));
+        }
+
+    }
+
+    public static void InTheFuture(this IGuardClause guardClause, DateTime input, string parameterName)
+    {
+        if (input > DateTime.Now)
+        {
+            throw new ArgumentException($"Input {parameterName} cannot be in the future.", parameterName);
+        }
+    }
 }
--- ConAppPlayingWithGuards/Models/Order.cs
+++ ConAppPlayingWithGuards/Models/Order.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConAppPlayingWithGuards.Models;
+﻿namespace ConAppPlayingWithGuards.Models;
 public class Order
 {
     public DateTime? OrderDate { get; set; }
