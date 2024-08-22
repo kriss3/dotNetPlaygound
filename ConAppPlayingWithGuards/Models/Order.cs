47bb875da4c1f416fbfd5c2767a05552c6d9de19
Kris <kris_s2@outlook.com>
2024-08-21 23:39:38 -0700

    kata: adding the rest of the models;

--- ConAppPlayingWithGuards/Models/Order.cs
+++ ConAppPlayingWithGuards/Models/Order.cs
@@ -7,7 +7,19 @@ using System.Threading.Tasks;
 namespace ConAppPlayingWithGuards.Models;
 public class Order
 {
-    public DateTime OrderDate { get; set; }
-    public Customer Customer { get; set; }
-    public List<OrderItem> OrderItems { get; set; }
+    public DateTime? OrderDate { get; set; }
+    public Customer? Customer { get; set; }
+    public List<OrderItem>? OrderItems { get; set; }
+}
+
+public class OrderItem
+{
+    public int? ProductId { get; set; }
+    public int? Quantity { get; set; }
+}
+
+public class Customer
+{
+    public int? CustomerId { get; set; }
+    public string? Name { get; set; }
 }
