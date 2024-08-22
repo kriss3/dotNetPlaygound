41e22aede0066d9c707c4cb40ac69955710c9d5b
Kris <kris_s2@outlook.com>
2024-08-21 23:38:12 -0700

    kata: adding models for my guards scenario;

---
+++ ConAppPlayingWithGuards/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppPlayingWithGuards.Models;
+public class Order
+{
+    public DateTime OrderDate { get; set; }
+    public Customer Customer { get; set; }
+    public List<OrderItem> OrderItems { get; set; }
+}
