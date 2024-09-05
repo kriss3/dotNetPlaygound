4dea2a834dc2d2ccfcde3c16771746bc31771a34
Kris <kris_s2@outlook.com>
2024-09-04 23:28:17 -0700

    kata: adding new project to play and learn about HttpClient lib: Refit;

---
+++ ConAppPlayingWithRefit/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+namespace ConAppPlayingWithRefit;
+
+public class Program
+{
+    static async Task Main()
+    {
+        WriteLine("Hello, World!");
+        await Task.CompletedTask;
+    }
+}
