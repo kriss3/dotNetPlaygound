8c3f2fcb4a1a8a0320c6ceb099629fc9ccf727c4
Kris <kris_s2@outlook.com>
2024-09-05 23:34:54 -0700

    kata: starting wiht interface and adding Money model;

---
+++ ConAppPlayingWithRefit/Model/Monkey.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConAppPlayingWithRefit.Model;
+public class Monkey
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Location { get; set; }
+    public string? ImageUrl { get; set; }
+}
---
+++ ConAppPlayingWithRefit/MonkeyClient/IMonkeyApi.cs
@@ -0,0 +1,15 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppPlayingWithRefit.MonkeyClient;
+public interface IMonkeyApi
+{
+    [Get("/monkeydata.json")]
+    Task<List<Monkey>> GetMonkeys();
+}
+
