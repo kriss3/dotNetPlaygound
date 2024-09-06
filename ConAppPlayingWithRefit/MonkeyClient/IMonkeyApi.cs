7261ecb5d8d27aed807b55b7d5df665585bf4388
Kris <kris_s2@outlook.com>
2024-09-05 23:35:48 -0700

    kata: fixing interface;

--- ConAppPlayingWithRefit/MonkeyClient/IMonkeyApi.cs
+++ ConAppPlayingWithRefit/MonkeyClient/IMonkeyApi.cs
@@ -1,10 +1,5 @@
-﻿using Refit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ConAppPlayingWithRefit.Model;
+using Refit;
 
 namespace ConAppPlayingWithRefit.MonkeyClient;
 public interface IMonkeyApi
