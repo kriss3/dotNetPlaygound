603a83e34f0fa3fd74d8bf034c7779d7cd3bbaad
Kris <kris_s2@outlook.com>
2024-09-06 21:55:32 -0700

    kata: Adding Api Factory;

---
+++ ConAppPlayingWithRefit/MonkeyClient/MonkeyApiFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Refit;
+
+namespace ConAppPlayingWithRefit.MonkeyClient;
+public static class MonkeyApiFactory
+{
+    public static IMonkeyApi CreateMonkeyApi(IServiceProvider serviceProvider)
+    {
+        var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+        var httpClient = httpClientFactory.CreateClient("monkeyClient");
+        return RestService.For<IMonkeyApi>(httpClient);
+    }
+}
