774ffd5045deb825f7c199535b02b6bca62e4299
Kris <kris_s2@outlook.com>
2024-09-08 23:24:34 -0700

    kata: interface clarification;

--- ConAppPlayingWithRefit/MonkeyClient/IMonkeyApi.cs
+++ ConAppPlayingWithRefit/MonkeyClient/IMonkeyApi.cs
@@ -7,6 +7,8 @@ public interface IMonkeyApi
     [Get("/monkeydata.json")]
     Task<List<Monkey>> GetMonkeys();
 
+
+    // Api is a public one but not mine so implementation of manipulation of the collection is limited;
     [Post("/monkeys")]
     Task<Monkey> AddMonkey([Body] Monkey monkey);
 
