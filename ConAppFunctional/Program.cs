bd61f909b601b697139d40d9533a83dc27ed0e3f
Kris <kris_s2@outlook.com>
2024-11-01 21:06:16 -0700

    kata: playing with ServiceError and Reuslt type

--- ConAppFunctional/Program.cs
+++ ConAppFunctional/Program.cs
@@ -1,7 +1,6 @@
 ﻿
 using Cova.Functional;
 using Cova.ServiceErrors.Errors;
-
 using static System.Console;
 
 namespace ConAppFunctional;
@@ -13,4 +12,42 @@ public class Program
 		await Task.CompletedTask;
 		WriteLine("Let's play with CS Functional!");
 	}
+
+	static async Task<Result<int, ServiceError>> PerformanceOperationAsync() 
+	{
+		//Delay
+		await Task.Delay(2000);
+		//Success
+		return Result.Success<int, ServiceError>(42);
+	}
+
+	static async Task<Result<int, ServiceError>> ErrorOperationAsync()
+	{
+		//Delay
+		await Task.Delay(2000);
+		//Error
+		return Result.Failure<int, ServiceError>(new ConcreteServiceError("Error", "Error message"));
+
+	}
+}
+
+public class ConcreteServiceError(string code, string message) : 
+	ServiceError(code, new Exception(message))
+{
+	public override string ErrorType { get; } = "ConcreteServiceError";
+	public override IEnumerable<ErrorCode> ErrorCodes { get; } = new List<ErrorCode>();
+
+	public override T Match<T>(
+		Func<PersistenceDataError, T> persistenceDataError,
+		Func<UnexpectedError, T> unexpectedError,
+		Func<ValidationError, T> validationError,
+		Func<MappingError, T> mappingError,
+		Func<AuthorizationError, T> authorizationError,
+		Func<AuthenticationError, T> authenticationError,
+		Func<CustomError, T> customError)
+	{
+		// Implement the match logic here
+		throw new NotImplementedException();
+	}
+
 }
