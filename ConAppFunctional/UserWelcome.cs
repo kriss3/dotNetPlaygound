224f1c596d9c79773f22ae64bbf859bcfbf14b09
Kris <kris_s2@outlook.com>
2024-11-13 23:22:00 -0800

    kata: attempt to work with more complex examples. UserWelcome case with map and mapFailure.

---
+++ ConAppFunctional/UserWelcome.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Cova.Functional;
+using Cova.ServiceErrors.Errors;
+
+namespace ConAppFunctional;
+public class UserWelcome
+{
+
+
+
+}
