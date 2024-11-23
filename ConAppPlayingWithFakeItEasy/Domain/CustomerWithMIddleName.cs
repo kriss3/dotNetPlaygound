fe17f44fd2d68e0a22b349cf2235bb924b2de461
Kris <kris_s2@outlook.com>
2024-11-23 15:57:34 -0800

    kata: adding autofixture and autofixture inlineAutoData;

---
+++ ConAppPlayingWithFakeItEasy/Domain/CustomerWithMIddleName.cs
@@ -0,0 +1,24 @@
+﻿namespace ConAppPlayingWithFakeItEasy.Domain;
+public class CustomerWithMIddleName
+{
+	public string? FirstName { get; set; }
+	public string? LastName { get; set; }
+	public string? MiddleName { get; set; }
+	public string FullName { get { return $"{FirstName} {MiddleName} {LastName}"; } }
+	public int Age { get; set; }
+	public string? Address { get; set; }
+
+	public CustomerWithMIddleName(
+		string? firstName, 
+		string? lastName, 
+		string? middleName,
+		int age, 
+		string? address)
+	{
+		FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
+		LastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
+		MiddleName = middleName ?? throw new ArgumentNullException(nameof(middleName));
+		Age = age;
+		Address = address ?? throw new ArgumentNullException(nameof(address));
+	}
+}
--- ConAppPlayingWithFakeItEasyTests/CustomerFullNameTests.cs
+++ ConAppPlayingWithFakeItEasyTests/CustomerFullNameTests.cs
@@ -1,5 +1,6 @@
-﻿using ConAppPlayingWithFakeItEasy.Domain;
-using FluentAssertions;
+﻿using AutoFixture;
+using AutoFixture.Xunit2;
+using ConAppPlayingWithFakeItEasy.Domain;
 
 namespace ConAppPlayingWithFakeItEasyTests;
 
@@ -10,9 +11,85 @@ public class CustomerFullNameTests
 	{
 		var sut = new Customer("John", "Doe", 30, "123 Main St");
 
-		var actual = sut.FullName;	
+		var actual = sut.FullName;
 
 		Assert.Equal("John Doe", actual);
 
+	}
+
+	[Theory]
+	[InlineData("Mark", "White", "Mark White")]
+	[InlineData("VS", "Code", "VS Code")]
+	public void FullNameReturnsExpected_v1(
+		string firstName,
+		string lastName,
+		string expected)
+	{
+		var sut = new Customer(firstName, lastName, 0, "Dummy Address");
+
+		var actual = sut.FullName;
+
+		Assert.Equal(expected, actual);
+
+	}
+
+	[Theory]
+	[InlineData("Mark", "White", "Mark White")]
+	[InlineData("VS", "Code", "VS Code")]
+	public void FullNameReturnsExpected_v2(
+		string firstName,
+		string lastName,
+		string expected)
+	{
+		var fixture = new Fixture();
+		var sut = fixture.Build<Customer>()
+			.With(x => x.FirstName, firstName)
+			.With(x => x.LastName, lastName)
+			.Create();
+
+		var actual = sut.FullName;
+
+		Assert.Equal(expected, actual);
+
+	}
+
+	[Theory]
+	[InlineData("Mark", "M", "White", "Mark M White")]
+	[InlineData("VS", "S", "Code", "VS S Code")]
+	public void FullNameWithMiddleNameReturnsExpected_v1(
+	string firstName, string middleName,
+	string lastName,
+	string expected)
+	{
+		var fixture = new Fixture();
+		var sut = fixture.Build<CustomerWithMIddleName>()
+			.With(x => x.FirstName, firstName)
+			
+			.With(x => x.LastName, lastName)
+			.With(x => x.MiddleName, middleName)
+			.Create();
+
+		var actual = sut.FullName;
+
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineAutoData("Mark", "M", "White", "Mark M White")]
+	[InlineAutoData("VS", "S", "Code", "VS S Code")]
+	public void FullNameWithMiddleNameReturnsExpected_v2(
+		string firstName, 
+		string middleName,
+		string lastName,
+		string expected,
+		CustomerWithMIddleName sut)
+	{
+		sut.FirstName = firstName;
+		sut.MiddleName = middleName;
+		sut.LastName = lastName;
+
+		var actual = sut.FullName;
+
+		Assert.Equal(expected, actual);
 	}
 }
