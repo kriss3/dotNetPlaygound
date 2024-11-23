efdd964c2ff291daf5d414f05c96bc2732d36d00
Kris <kris_s2@outlook.com>
2024-11-23 15:14:34 -0800

    kata: prep data to work with Autofixture.

---
+++ ConAppPlayingWithFakeItEasy/Domain/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppPlayingWithFakeItEasy.Domain;
+public class Customer
+{
+	public string? FirstName { get; set; }
+	public string? LastName { get; set; }
+	public string FullName { get { return $"{FirstName} {LastName}"; } }
+	public int Age { get; set; }
+	public string? Address { get; set; }
+
+	public Customer(string? firstName, string? lastName, int age, string? address)
+	{
+		FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
+		LastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
+		Age = age;
+		Address = address ?? throw new ArgumentNullException(nameof(address));
+	}
+}
