bece16f77a82ee0d8bd3f9f3687f98e8a9ce9fae
Kris <kris_s2@outlook.com>
2024-11-23 15:27:22 -0800

    kata: removing unnecessary liberaries from the main project. Adding a new project that is of type xunit and this one will have all the necessary libraries for unit tests: xUnit, FluentAssertions, Autofixture and FakeItEasy;

---
+++ ConAppPlayingWithFakeItEasyTests/CustomerFullNameTests.cs
@@ -0,0 +1,18 @@
+﻿using ConAppPlayingWithFakeItEasy.Domain;
+using FluentAssertions;
+
+namespace ConAppPlayingWithFakeItEasyTests;
+
+public class CustomerFullNameTests
+{
+	[Fact]
+	public void FullNameReturnsExpected()
+	{
+		var sut = new Customer("John", "Doe", 30, "123 Main St");
+
+		var actual = sut.FullName;	
+
+		Assert.Equal("John Doe", actual);
+
+	}
+}
