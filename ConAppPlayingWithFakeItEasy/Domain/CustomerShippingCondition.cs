dd802d7558b898009516cf71ea49c8cdbd7030a1
Kris <kris_s2@outlook.com>
2024-11-24 23:33:20 -0800

    kata: Adding implementation to the condition class

--- ConAppPlayingWithFakeItEasy/Domain/CustomerShippingCondition.cs
+++ ConAppPlayingWithFakeItEasy/Domain/CustomerShippingCondition.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConAppPlayingWithFakeItEasy.Domain;
+﻿namespace ConAppPlayingWithFakeItEasy.Domain;
 public class CustomerShippingCondition
 {
+
+	public bool Check(CustomerWithAddress cust) 
+	{
+		return cust.AddressList.Any() &&
+			cust.Age > 21;
+	}
 }
