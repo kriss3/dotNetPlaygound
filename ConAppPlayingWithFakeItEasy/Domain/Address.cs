b7783bb3ddfa697a848b3f0395c2fed7a5940890
Kris <kris_s2@outlook.com>
2024-11-24 23:25:52 -0800

    kata: adding details to the Address class

--- ConAppPlayingWithFakeItEasy/Domain/Address.cs
+++ ConAppPlayingWithFakeItEasy/Domain/Address.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConAppPlayingWithFakeItEasy.Domain;
-internal class Address
+﻿namespace ConAppPlayingWithFakeItEasy.Domain;
+public class Address
 {
+	public string? Street { get; set; }
+	public string? City { get; set; }
+	public string? State { get; set; }
+	public string? ZipCode { get; set; }
 }
