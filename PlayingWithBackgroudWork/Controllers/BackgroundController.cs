2a4367e1d00f814d67cf0fb8139c8dd8ee3d0018
Kris <kris_s2@outlook.com>
2024-12-09 23:09:19 -0800

    kata: Adding Background controller;

---
+++ PlayingWithBackgroudWork/Controllers/BackgroundController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlayingWithBackgroudWork.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class BackgroundController(SampleData data) : ControllerBase
+{
+	private readonly SampleData _data = data;
+
+	[HttpGet]
+	public IActionResult Get()
+	{
+		return Ok(_data.Data.Order());
+	}
+}
