34c2dfd3e1d47aca8991b1c1683b9dc2887f1ea0
Kris <kris_s2@outlook.com>
2024-12-09 23:06:47 -0800

    kata: Adding swagger lib;

--- PlayingWithBackgroudWork/Program.cs
+++ PlayingWithBackgroudWork/Program.cs
@@ -1,5 +1,7 @@
 
+using Microsoft.OpenApi.Models;
 using PlayingWithBackgroudWork;
+using PlayingWithBackgroudWork.Services;
 
 namespace WebAppPlayingWithBackgroudWork;
 
@@ -11,19 +13,24 @@ public class Program
 
 		builder.Services.AddControllers();
 		builder.Services.AddOpenApi();
+		builder.Services.AddSwaggerGen(c => // Add this configuration
+		{
+			c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+		});
 		builder.Services.AddSingleton<SampleData>();
+		builder.Services.AddHostedService<BackgroundRefresh>();
 
 		var app = builder.Build();
 
 		if (app.Environment.IsDevelopment())
 		{
 			app.MapOpenApi();
+			app.UseSwagger();
 		}
 
 		app.UseHttpsRedirection();
 
 		app.UseAuthorization();
-
 
 		app.MapControllers();
 
