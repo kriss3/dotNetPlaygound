bda3539862991f5cbc35621e3b15da6427bd8edf
Kris <kris_s2@outlook.com>
2025-01-27 21:37:47 -0800

    kata: cleanup

--- src/ConAppFunctional/BindHelper.cs
+++ src/ConAppFunctional/BindHelper.cs
@@ -1,10 +1,4 @@
 ﻿using Cova.Functional;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 using static System.Console;
 namespace ConAppFunctional;
 public class BindHelper
