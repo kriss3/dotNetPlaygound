64f4d4e46b45a73c5c6ca7af2300b61e4a969d67
Kris <kris_s2@outlook.com>
2025-01-28 23:16:15 -0800

    kata: adding LocationId primitive implementation.

--- PlayingWithDDD.App/src/DomainLayer/Primitives/LocationId.cs
+++ PlayingWithDDD.App/src/DomainLayer/Primitives/LocationId.cs
@@ -1,3 +1,20 @@
 ﻿namespace PlayingWithDDD.App.src.DomainLayer.Primitives;
 
-public record LocationId(int Value);
+public record LocationId 
+{
+	public int Value { get; }
+
+	private LocationId(int value)
+	{
+		Value = value;
+	}
+
+	public static LocationId Create(int value)
+	{
+		if (value <= 0)
+		{
+			throw new ArgumentException("LocationId cannot be less than or equal to 0", nameof(value));
+		}
+		return new LocationId(value);
+	}
+}
