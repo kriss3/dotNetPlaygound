8c54b29b61357a1e942cb0c4256ffa67e2f9057e
Kris <kris_s2@outlook.com>
2025-01-28 23:04:44 -0800

    kata: attempt to add a domain details

---
+++ PlayingWithDDD.App/src/DomainLayer/Interfaces/IExternalApiClient.cs
@@ -0,0 +1,13 @@
+﻿
+using Cova.Functional;
+using Cova.ServiceErrors.Errors;
+using PlayingWithDDD.App.src.InfraLayer;
+
+namespace PlayingWithDDD.App.src.DomainLayer.Interfaces;
+
+public interface IExternalApiClient
+{
+	Task<Result<ValidationResource, ServiceError>> CallExternalApiAsync(CompanyId companyId, LocationId locationId);
+}
+
+public record class ValidationResource(string Message, bool IsValid);
