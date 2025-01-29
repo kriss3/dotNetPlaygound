396f8a81e5559d5353f32a6da0daadcd3def28c0
Kris <kris_s2@outlook.com>
2025-01-28 23:30:43 -0800

    kata: Adding implementation of Application layer.

---
+++ PlayingWithDDD.App/src/ApplicationLayer/CreateRequestHandlerFactory.cs
@@ -0,0 +1,24 @@
+﻿using PlayingWithDDD.App.src.ApplicationLayer.Models;
+using PlayingWithDDD.App.src.DomainLayer.Interfaces;
+using PlayingWithDDD.App.src.DomainLayer.Primitives;
+
+using Cova.Functional;
+using Cova.ServiceErrors.Errors;
+
+namespace PlayingWithDDD.App.src.ApplicationLayer;
+
+public static class CreateRequestHandlerFactory
+{
+	public delegate Task<Result<ValidationResource, ServiceError>> CreateRequestHandler(CreateRequestCommand command);
+	public static CreateRequestHandler Create(IExternalApiClient apiClient) =>
+		async command =>
+		{
+			// Convert to domain primitives
+			var companyId = CompanyId.Create(command.CompanyId);
+			var locationId = LocationId.Create(command.LocationId);
+
+			// Pass to the domain service (via abstraction)
+			return await apiClient.CallExternalApiAsync(companyId, locationId);
+		};
+}
+
---
+++ PlayingWithDDD.App/src/ApplicationLayer/Models/CreateRequestCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace PlayingWithDDD.App.src.ApplicationLayer.Models;
+
+public record CreateRequestCommand(int CompanyId, int LocationId);
