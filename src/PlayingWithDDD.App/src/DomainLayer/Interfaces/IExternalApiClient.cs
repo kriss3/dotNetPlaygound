98397f3e2e4fe06e1ebc737e2bcfdef9e6360117
Kris <kris_s2@outlook.com>
2025-01-29 21:54:37 -0800

    kata: cleanup

--- src/PlayingWithDDD.App/src/DomainLayer/Interfaces/IExternalApiClient.cs
+++ src/PlayingWithDDD.App/src/DomainLayer/Interfaces/IExternalApiClient.cs
@@ -2,7 +2,6 @@
 using Cova.Functional;
 using Cova.ServiceErrors.Errors;
 using PlayingWithDDD.App.src.DomainLayer.Primitives;
-using PlayingWithDDD.App.src.InfraLayer;
 
 namespace PlayingWithDDD.App.src.DomainLayer.Interfaces;
 
