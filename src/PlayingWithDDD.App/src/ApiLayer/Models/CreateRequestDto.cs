d17686c4a952fada4a75017248bc7723eb12dea6
Kris <kris_s2@outlook.com>
2025-01-30 23:03:50 -0800

    kata: Working on Api Layer

---
+++ src/PlayingWithDDD.App/src/ApiLayer/Models/CreateRequestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace PlayingWithDDD.App.src.ApiLayer.Models;
+
+public record CreateRequestDto
+{
+	public int CompanyId { get; init; }
+	public Guid LocationId { get; init; }
+}
---
+++ src/PlayingWithDDD.App/src/Controllers/RequestsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlayingWithDDD.App.src.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class RequestsController : ControllerBase
+{
+}
