0f315af64f30dcb6d8fe6bb837374b1f4fbea92a
Kris <kris_s2@outlook.com>
2025-01-30 23:05:31 -0800

    kata: Adding primary ctor.

--- src/PlayingWithDDD.App/src/Controllers/RequestsController.cs
+++ src/PlayingWithDDD.App/src/Controllers/RequestsController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PlayingWithDDD.App.src.ApplicationLayer;
+using PlayingWithDDD.App.src.DomainLayer.Interfaces;
+using static PlayingWithDDD.App.src.ApplicationLayer.CreateRequestHandlerFactory;
 
 namespace PlayingWithDDD.App.src.Controllers;
 [Route("api/[controller]")]
 [ApiController]
-public class RequestsController : ControllerBase
+public class RequestsController(IExternalApiClient apiClient) : ControllerBase
 {
+	// Bind the API client to the handler
+	private readonly CreateRequestHandler _handler = 
+		CreateRequestHandlerFactory.Create(apiClient);
 }
