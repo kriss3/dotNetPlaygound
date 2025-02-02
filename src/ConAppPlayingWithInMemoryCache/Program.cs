baf4a2650b0b19d39db3041e2bfcb587368ceea0
Kris <kris_s2@outlook.com>
2025-02-01 16:36:36 -0800

    kata: adding infra for playing with InMemory cache.

---
+++ src/ConAppPlayingWithInMemoryCache/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using static System.Console;
+
+namespace ConAppPlayingWithInMemoryCache;
+
+public class Program
+{
+	static Task Main()
+	{
+		WriteLine("Hello, World!");
+		return Task.CompletedTask;
+	}
+}
