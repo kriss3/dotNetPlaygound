83044b6e3c159a7b62d48411039dcda433f644d5
Kris <kris_s2@outlook.com>
2025-02-01 16:59:56 -0800

    kata: Adding Product model.

---
+++ src/ConAppPlayingWithInMemoryCache/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ConAppPlayingWithInMemoryCache.Models;
+public record Product
+{
+	[JsonPropertyName("id")]
+	public int Id { get; init; }
+
+	[JsonPropertyName("quantityPerUnit")]
+	public string? QuantityPerUnit { get; set; }
+
+	[JsonPropertyName("unitPrice")]
+	public decimal? UnitPrice { get; set; }
+
+	[JsonPropertyName("name")]
+	public string? Name { get; set; }
+}
