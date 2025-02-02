0ab1b822c59e4f8a000c665301a78ed80a4cfd83
Kris <kris_s2@outlook.com>
2025-02-02 15:49:26 -0800

    kata: updating Helper class with non-cache method implemention.

--- src/ConAppPlayingWithInMemoryCache/Services/MemoryCacheHelper.cs
+++ src/ConAppPlayingWithInMemoryCache/Services/MemoryCacheHelper.cs
@@ -1,9 +1,12 @@
 ﻿using ConAppPlayingWithInMemoryCache.Models;
+using Microsoft.Extensions.Caching.Memory;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
 
 namespace ConAppPlayingWithInMemoryCache.Services;
 
@@ -12,17 +15,38 @@ public interface IMemoryCacheHelper
 	Task<List<Product>> GetProductsWithCache();
 	Task<List<Product>> GetProducts();
 }
-public class MemoryCacheHelper : IMemoryCacheHelper
+public class MemoryCacheHelper(IMemoryCache memCache) : IMemoryCacheHelper
 {
+	private readonly IMemoryCache _memoryCache = memCache;
+
 	public List<Product>? Products { get; set; }
 
-	public Task<List<Product>> GetProducts()
+	public async Task<List<Product>> GetProducts()
 	{
-		throw new NotImplementedException();
+		try
+		{
+			HttpClient client = new();
+			var baseUrl = GetBaseUrl();
+			var endpoint = $"{baseUrl}/products";
+			var streamTask = client.GetStreamAsync(endpoint);
+			var products = await JsonSerializer.DeserializeAsync<List<Product>>(await streamTask);
+			return products!;
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"An error occurred while fetching products: {ex.Message}");
+			return new List<Product>();
+		}
+	}
+
+	private static string GetBaseUrl()
+	{
+		return @"https://northwind.now.sh/api";
 	}
 
 	public Task<List<Product>> GetProductsWithCache()
 	{
+		// var products = _memoryCache.Get<List<Product>>("products");
 		throw new NotImplementedException();
 	}
 }
