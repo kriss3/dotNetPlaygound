ae4fab37e5d5600bc87ad7aa415e735ee7a7cb85
Kris <kris_s2@outlook.com>
2025-02-03 22:05:06 -0800

    kata: cleanup.

--- src/ConAppPlayingWithInMemoryCache/Services/MemoryCacheHelper.cs
+++ src/ConAppPlayingWithInMemoryCache/Services/MemoryCacheHelper.cs
@@ -1,12 +1,7 @@
 ﻿using ConAppPlayingWithInMemoryCache.Models;
 using Microsoft.Extensions.Caching.Memory;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 using static System.Console;
 
