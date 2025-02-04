8f8e14efd7fd4929c5d1b44d989354d663d897c0
Kris <kris_s2@outlook.com>
2025-02-03 22:03:03 -0800

    kata: finishing the wrapper for getProductOrCache.

--- src/ConAppPlayingWithInMemoryCache/Services/MemoryCacheHelper.cs
+++ src/ConAppPlayingWithInMemoryCache/Services/MemoryCacheHelper.cs
@@ -69,15 +69,16 @@ public class MemoryCacheHelper(IMemoryCache memCache) : IMemoryCacheHelper
 		return @"https://northwind.now.sh/api";
 	}
 
-	public Task<List<Product>> GetProductsWithCache()
+	public async Task<List<Product>> GetProductsWithCache()
 	{
 		var t1 = Stopwatch.StartNew();
 		t1.Start();
-		var products = _memoryCache.Get<List<Product>>("products");
+		var products = await GetProductsOrCache();
 
 		t1.Stop();
 		var elapsed = t1.ElapsedMilliseconds;
-		return products == null ? GetProducts() : Task.FromResult(products); 
+		WriteLine($"Elapsed time: {elapsed / 1000.0} seconds");
+		return products;
 	}
 
 	private void DisplayProducts(List<Product> products)
