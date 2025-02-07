34bc40936fa61df35acfa75b578ba5271683c3ce
Kris <kris_s2@outlook.com>
2025-02-06 23:35:26 -0800

    kata: fixing references

--- src/ConAppPlayingWithFundamentals/Singleton.cs
+++ src/ConAppPlayingWithFundamentals/Singleton.cs
@@ -1,34 +1,34 @@
 ﻿using static System.Console;
 
-namespace ConAppsExercises;
+namespace ConAppPlayingWithFundamentals;
 
 public sealed class Singleton
 {
-    private static Singleton _instance = null;
-    private static readonly object padLock = new object();
+	private static Singleton _instance = null;
+	private static readonly object padLock = new object();
 
-    private Singleton()
-    {
-        WriteLine($"Creating new instance !!!");
-    }
+	private Singleton()
+	{
+		WriteLine($"Creating new instance !!!");
+	}
 
-    public static Singleton Instance
-    {
-        get
-        {
-            lock (padLock)
-            {
-                if (_instance == null)
-                {
-                    _instance = new Singleton();
-                }
-                else
-                {
-                    WriteLine($"Using excising instance !!!");
-                }
-                
-                return _instance;
-            }
-        }
-    }
+	public static Singleton Instance
+	{
+		get
+		{
+			lock (padLock)
+			{
+				if (_instance == null)
+				{
+					_instance = new Singleton();
+				}
+				else
+				{
+					WriteLine($"Using excising instance !!!");
+				}
+
+				return _instance;
+			}
+		}
+	}
 }
--- src/TestingUnitTests/BankAccountTests.cs
+++ src/TestingUnitTests/BankAccountTests.cs
@@ -1,37 +1,34 @@
-﻿using System;
+﻿using ConAppsExercises;
+using System;
 using Xunit;
 
-using ConAppsExercises;
+namespace TestingUnitTests;
 
-namespace TestingUnitTests 
+	public class BankAccountTests
 {
-    public class BankAccountTests
+    [Fact]
+    public void TestExceptionThrowing()
     {
-        [Fact]
-        public void TestExceptionThrowing()
+        //setup
+        BankAccount ba = new("Kris", 0);
+        //init
+        try
         {
-            //setup
-            BankAccount ba = new("Kris", 0);
-            //init
-            try
-            {
-                ba.Debit(2);
-            }
-            catch (ArgumentOutOfRangeException e)
-            {
-                Assert.Contains(e.Message, "Amount <=0 or Amount > Balanc");
-                return;
-            }
-            catch (Exception e)
-            {
-                Assert.Contains(e.Message, "Balance is 0");
-                return;
-            }
-            //assert
-            Assert.Fail("No exception was thrown.");
+            ba.Debit(2);
         }
+        catch (ArgumentOutOfRangeException e)
+        {
+            Assert.Contains(e.Message, "Amount <=0 or Amount > Balanc");
+            return;
+        }
+        catch (Exception e)
+        {
+            Assert.Contains(e.Message, "Balance is 0");
+            return;
+        }
+        //assert
+        Assert.Fail("No exception was thrown.");
     }
-
 }
 
 
