3040aeb2243c2ba9abbd443ce3319c2c116bfc92
Kris <kris_s2@outlook.com>
2025-02-07 23:04:28 -0800

    kata: cleanup

--- src/ConAppPlayingWithFundamentals/Program.cs
+++ src/ConAppPlayingWithFundamentals/Program.cs
@@ -6,7 +6,6 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
