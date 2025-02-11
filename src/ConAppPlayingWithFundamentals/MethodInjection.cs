8c3cb22dda67f7bf91ccaf4e9c92f4b55909289a
Kris <kris_s2@outlook.com>
2025-02-10 23:17:07 -0800

    kata: more fixes.

--- src/ConAppPlayingWithFundamentals/MethodInjection.cs
+++ src/ConAppPlayingWithFundamentals/MethodInjection.cs
@@ -1,37 +1,37 @@
 ﻿using static System.Console;
 
-namespace ConAppsExercises;
+namespace ConAppPlayingWithFundamentals;
 
 public interface IServiceBroker
 {
-    void Serve();
+	void Serve();
 }
 
 public class ServiceProvider : IServiceBroker
 {
-    public void Serve()
-    {
-        WriteLine($"Service started ...");
-    }
+	public void Serve()
+	{
+		WriteLine($"Service started ...");
+	}
 }
 
 public class ServiceConsumer
 {
-    private IServiceBroker sb;
-    public void Start(IServiceBroker s)
-    {
-        sb = s;
-        WriteLine("Service Caller");
-        sb.Serve();
-    }
-      
+	private IServiceBroker sb;
+	public void Start(IServiceBroker s)
+	{
+		sb = s;
+		WriteLine("Service Caller");
+		sb.Serve();
+	}
+
 }
 
 class MethodInjection
 {
-    public MethodInjection()
-    {
-        ServiceConsumer sc = new ServiceConsumer();
-        sc.Start(new ServiceProvider());
-    }
+	public MethodInjection()
+	{
+		ServiceConsumer sc = new();
+		sc.Start(new ServiceProvider());
+	}
 }
