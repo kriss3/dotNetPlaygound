fc020661f58d29c96277c64d7e9ff29dcf72b0e1
Kris <kris_s2@outlook.com>
2025-02-11 22:18:17 -0800

    kata: moving more method related to string manipulations.

--- src/ConAppPlayingWithFundamentals/Program.cs
+++ src/ConAppPlayingWithFundamentals/Program.cs
@@ -136,20 +136,20 @@ class Program
 				break;
 			case "26":
 				var word = "konstantynopolitanczykowna";
-				ReverseString(ref word);
+				StringOperations.ReverseString(ref word);
 				break;
 			case "27":
 				var sentence = "Ala ma kota";
-				ReverseSentence(sentence);
+				StringOperations.ReverseSentence(sentence);
 				break;
 			case "28":
 				var exampleWord = "HellO";
-				RemoveVowels(exampleWord);
+				StringOperations.RemoveVowels(exampleWord);
 				break;
 			case "29":
 				var inputExample1 = "ABCDE";
 				var inputExample2 = "DC";
-				FindDelimiterOccurrence(inputExample1, inputExample2);
+				StringOperations.FindDelimiterOccurrence(inputExample1, inputExample2);
 				break;
 			case "30":
 				CardDeckShuffler();
@@ -675,80 +675,6 @@ class Program
 	protected virtual void GetCat()
 	{
 		WriteLine($"This is inside method: {MethodBase.GetCurrentMethod().Name}()");
-	}
-
-	private static string ReverseString(ref string testString)
-	{
-		var sb = new StringBuilder();
-		if (!string.IsNullOrEmpty(testString))
-		{
-			var cTest = testString.ToCharArray();
-			for (int i = cTest.Length - 1; i >= 0; i--)
-			{
-				sb.Append(cTest[i]);
-			}
-		}
-		WriteLine(sb.ToString());
-		return sb.ToString();
-	}
-
-	private static string[] ReverseSentence(string sentence)
-	{
-		var arr = sentence.ToCharArray();
-		var result = new List<string>();
-		var t = string.Empty;
-		for (int i = 0; i < arr.Length; i++)
-		{
-			var temp = arr[i];
-			if (temp != ' ')
-			{
-				t += sentence[i];
-			}
-			else
-			{
-				result.Add(t);
-				t = string.Empty;
-			}
-		}
-		result.Add(t);
-		t = string.Empty;
-		result.ForEach(x => WriteLine(x));
-		return [.. result];
-	}
-
-	private static void RemoveVowels(string cTest)
-	{
-		var test = cTest.Where(c => "aeiouAEIOU".Contains(c)).Distinct();
-
-		foreach (var item in test)
-		{
-			WriteLine($"Test word: {cTest} contains vowels: {item}");
-		}
-
-		if (!test.Any())
-			WriteLine($"No vowels found in {cTest}");
-
-	}
-
-	private static long FindDelimiterOccurrence(string s1, string s2)
-	{
-		var s1Arr = s1.ToCharArray();
-		var s2Arr = s2.ToCharArray();
-		var res = 0;
-		for (int i = 0; i < s1Arr.Length; i++)
-		{
-			for (int j = 0; j < s2Arr.Length; j++)
-			{
-				if (s1Arr[i] == s2Arr[j])
-				{
-					var tempIndex = Array.IndexOf(s1Arr, s1Arr[i]);
-					if (tempIndex > res)
-						res = tempIndex;
-				}
-			}
-		}
-		WriteLine(res + 1);
-		return res + 1;
 	}
 
 	private static void CardDeckShuffler()
--- src/ConAppPlayingWithFundamentals/Services/StringService/StringOperations.cs
+++ src/ConAppPlayingWithFundamentals/Services/StringService/StringOperations.cs
@@ -1,4 +1,7 @@
-﻿using static System.Console;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
 
 namespace ConAppPlayingWithFundamentals.Services.StringService;
 public static class StringOperations
@@ -36,11 +39,86 @@ public static class StringOperations
 			WriteLine($"Word {s} is not a palindrome");
 		}
 	}
+
 	public static void Run2(int val)
 	{
 		for (int i = val; i >= 0; i--)
 		{
 			WriteLine($"Back iteration: {i}  == value: {i}");
 		}
+	}
+
+	public static string ReverseString(ref string testString)
+	{
+		var sb = new StringBuilder();
+		if (!string.IsNullOrEmpty(testString))
+		{
+			var cTest = testString.ToCharArray();
+			for (int i = cTest.Length - 1; i >= 0; i--)
+			{
+				sb.Append(cTest[i]);
+			}
+		}
+		WriteLine(sb.ToString());
+		return sb.ToString();
+	}
+
+	public static string[] ReverseSentence(string sentence)
+	{
+		var arr = sentence.ToCharArray();
+		var result = new List<string>();
+		var t = string.Empty;
+		for (int i = 0; i < arr.Length; i++)
+		{
+			var temp = arr[i];
+			if (temp != ' ')
+			{
+				t += sentence[i];
+			}
+			else
+			{
+				result.Add(t);
+				t = string.Empty;
+			}
+		}
+		result.Add(t);
+		t = string.Empty;
+		result.ForEach(x => WriteLine(x));
+		return [.. result];
+	}
+
+	public static void RemoveVowels(string cTest)
+	{
+		var test = cTest.Where(c => "aeiouAEIOU".Contains(c)).Distinct();
+
+		foreach (var item in test)
+		{
+			WriteLine($"Test word: {cTest} contains vowels: {item}");
+		}
+
+		if (!test.Any())
+			WriteLine($"No vowels found in {cTest}");
+
+	}
+
+	public static long FindDelimiterOccurrence(string s1, string s2)
+	{
+		var s1Arr = s1.ToCharArray();
+		var s2Arr = s2.ToCharArray();
+		var res = 0;
+		for (int i = 0; i < s1Arr.Length; i++)
+		{
+			for (int j = 0; j < s2Arr.Length; j++)
+			{
+				if (s1Arr[i] == s2Arr[j])
+				{
+					var tempIndex = Array.IndexOf(s1Arr, s1Arr[i]);
+					if (tempIndex > res)
+						res = tempIndex;
+				}
+			}
+		}
+		WriteLine(res + 1);
+		return res + 1;
 	}
 }
