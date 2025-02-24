346703f38d5735b236f0a601833cc291c2ffe466
Kris <kris_s2@outlook.com>
2025-02-23 16:57:54 -0800

    kata: addng explicit implementation of ImageProxy. This would wrap the intended object and expose DisplayImage method to the public. Reminder DisplayImage can be access/executed directly from RealImage but also via the Proxy object: ImageProxy.

--- ConAppWorkingWithProxyPattern/Program.cs
+++ ConAppWorkingWithProxyPattern/Program.cs
@@ -39,3 +39,26 @@ public class RealImage : IImage
 	}
 }
 
+public class ImageProxy : IImage
+{
+	// this class should implement a common interface and "wrap" the intended object.
+	private readonly string _fileName;
+	private RealImage? _realImage;
+
+	public ImageProxy(string fileName)
+	{
+		_fileName = fileName;
+	}
+
+	public void DisplayImage()
+	{
+		_realImage ??= new RealImage(_fileName);
+		//if (_realImage is null)
+		//{
+		//	_realImage = new RealImage(_fileName);
+		//}
+		//_realImage.DisplayImage();
+	}	
+
+}
+
