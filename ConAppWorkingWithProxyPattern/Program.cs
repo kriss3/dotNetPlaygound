9cca87e06c6429fd0afdbd51f252bb6850d80665
Kris <kris_s2@outlook.com>
2025-02-23 16:49:03 -0800

    kata: rewriting ctor for RealImage implementation.

--- ConAppWorkingWithProxyPattern/Program.cs
+++ ConAppWorkingWithProxyPattern/Program.cs
@@ -18,13 +18,24 @@ public interface IImage
 }
 
 
-public class RealImage(string fileName) : IImage 
+public class RealImage : IImage 
 {
-	private readonly string _fileName = fileName;
+	private readonly string _fileName;
+
+	public RealImage(string fileName)
+	{
+		_fileName = fileName;
+		LoadImageFromDisk();
+	}
 
 	public void DisplayImage()
 	{
 		WriteLine($"Displaying {_fileName}");
+	}
+
+	private void LoadImageFromDisk()
+	{
+		WriteLine($"Loading {_fileName} from Disk()");
 	}
 }
 
