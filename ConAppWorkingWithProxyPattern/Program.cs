6b3a91c4db1f8f2cec3b435fc0d4e89eee1d63fd
Kris <kris_s2@outlook.com>
2025-02-23 16:39:50 -0800

    kata: Adding new proj to play and learn about Proxy desing pattern.

---
+++ ConAppWorkingWithProxyPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConAppPlayingWithProxyPattern;
+
+using static System.Console;
+
+public class Program
+{
+	static Task Main()
+	{
+		WriteLine("Working with Proxy Design Pattern!");
+		return Task.CompletedTask;
+	}
+}
