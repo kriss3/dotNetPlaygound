95b4b6970fc7f527604a2e9335707cc5723b8f2c
Kris <kris_s2@outlook.com>
2025-02-24 23:23:11 -0800

    kata: adding implemention for CachedApiProxy

--- ConAppWorkingWithProxyPattern/ApiService/CachedApiProxy.cs
+++ ConAppWorkingWithProxyPattern/ApiService/CachedApiProxy.cs
@@ -10,8 +10,23 @@ using static System.Console;
 namespace ConAppPlayingWithProxyPattern.ApiService;
 public class CachedApiProxy : IApiService
 {
-	public Task<string> GetDataAsync(string url)
+	private readonly RealApiService _realApiService;
+	private readonly Dictionary<string, (string Data, DateTime Expiry)> _cachedData = [];
+	private readonly TimeSpan _cacheDuration = TimeSpan.FromSeconds(10);
+
+	public async Task<string> GetDataAsync(string url)
 	{
-		throw new NotImplementedException();
+		if (_cachedData.TryGetValue(url, out var cachedResponse) && DateTime.UtcNow < cachedResponse.Expiry) 
+		{
+			WriteLine("Returning cached data");
+			return cachedResponse.Data;
+		}
+		else
+		{
+			WriteLine("Fetching data from external source");
+			var data = await _realApiService.GetDataAsync(url);
+			_cachedData[url] = (data, DateTime.UtcNow.Add(_cacheDuration));
+			return data;
+		}
 	}
 }
