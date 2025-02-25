d45dae2dedfba40f0139b1ec849c2c9189413e13
Kris <kris_s2@outlook.com>
2025-02-24 23:16:40 -0800

    kata: adding cached version of IApiService.

---
+++ ConAppWorkingWithProxyPattern/ApiService/CachedApiProxy.cs
@@ -0,0 +1,17 @@
+﻿using ConAppPlayingWithProxyPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace ConAppPlayingWithProxyPattern.ApiService;
+public class CachedApiProxy : IApiService
+{
+	public Task<string> GetDataAsync(string url)
+	{
+		throw new NotImplementedException();
+	}
+}
