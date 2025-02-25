7f6b13fd10ed5afb908e8df2618dfab1debf11c3
Kris <kris_s2@outlook.com>
2025-02-24 23:23:34 -0800

    kata: cleanup

--- ConAppWorkingWithProxyPattern/ApiService/CachedApiProxy.cs
+++ ConAppWorkingWithProxyPattern/ApiService/CachedApiProxy.cs
@@ -1,9 +1,4 @@
 ﻿using ConAppPlayingWithProxyPattern.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using static System.Console;
 
