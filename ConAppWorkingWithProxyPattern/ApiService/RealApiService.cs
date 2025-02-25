601510be446a984a72a0f68c73b2c754cb8886e4
Kris <kris_s2@outlook.com>
2025-02-24 22:55:23 -0800

    kata: first take at real implemention of IApiService.

---
+++ ConAppWorkingWithProxyPattern/ApiService/RealApiService.cs
@@ -0,0 +1,21 @@
+﻿using ConAppPlayingWithProxyPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace ConAppPlayingWithProxyPattern.ApiService;
+public class RealApiService : IApiService
+{
+	private readonly HttpClient _httpClient;
+
+	public Task<string> GetDataAsync(string url)
+	{
+		WriteLine($"Fetching data from external source, URL: {url}");
+		var response = _httpClient.GetStringAsync(url);
+		return response;
+	}
+}
