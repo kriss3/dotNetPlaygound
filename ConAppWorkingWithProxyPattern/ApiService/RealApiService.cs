bf4b576f3539bea34bb39a01ad7c7c9d62bf063c
Kris <kris_s2@outlook.com>
2025-02-24 22:56:16 -0800

    kata: dealing with nullable types.

--- ConAppWorkingWithProxyPattern/ApiService/RealApiService.cs
+++ ConAppWorkingWithProxyPattern/ApiService/RealApiService.cs
@@ -1,9 +1,4 @@
 ﻿using ConAppPlayingWithProxyPattern.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using static System.Console;
 
@@ -11,6 +6,11 @@ namespace ConAppPlayingWithProxyPattern.ApiService;
 public class RealApiService : IApiService
 {
 	private readonly HttpClient _httpClient;
+
+	public RealApiService(HttpClient httpClient)
+	{
+		_httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+	}
 
 	public Task<string> GetDataAsync(string url)
 	{
