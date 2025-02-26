a7bc2870375e8f72414f4aa3afec158d5832ec93
Kris <kris_s2@outlook.com>
2025-02-25 22:17:58 -0800

    kata: adding Redis library.

---
+++ ConAppWorkingWithProxyPattern/ApiService/RedisCachedApiService.cs
@@ -0,0 +1,13 @@
+﻿using ConAppPlayingWithProxyPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppPlayingWithProxyPattern.ApiService;
+public class RedisCachedApiService : IApiService
+{
+
+
+}
