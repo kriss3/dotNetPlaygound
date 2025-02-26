01643d5fee7764f9fe4b070fbe27f8240797fa2b
Kris <kris_s2@outlook.com>
2025-02-25 22:40:31 -0800

    kata: adding implementaiont of RedisCachedApiService...

--- ConAppWorkingWithProxyPattern/ApiService/RedisCachedApiService.cs
+++ ConAppWorkingWithProxyPattern/ApiService/RedisCachedApiService.cs
@@ -14,8 +14,26 @@ public class RedisCachedApiService : IApiService
 	private readonly IDatabase _redisDatabase;
 	private readonly TimeSpan _cacheDuration = TimeSpan.FromSeconds(30);
 
-	public Task<string> GetDataAsync(string url)
+	public RedisCachedApiService()
 	{
-		throw new NotImplementedException();
+		var redis = ConnectionMultiplexer.Connect("add azure Redis connection string, after u set it up...");
+		_redisDatabase = redis.GetDatabase();
+		_realApiService = new RealApiService(new HttpClient());
+	}
+
+	public async Task<string> GetDataAsync(string url)
+	{
+		string cacheKey = $"api_cache:{url}";
+		var cachedData = await _redisDatabase.StringGetAsync(cacheKey);
+
+		if (!cachedData.IsNullOrEmpty)
+		{
+			Console.WriteLine($"Returning cached data from Redis for: {url}");
+			return cachedData.ToString();
+		}
+
+		var data = await _realApiService.GetDataAsync(url);
+		await _redisDatabase.StringSetAsync(cacheKey, data, _cacheDuration);
+		return data;
 	}
 }
