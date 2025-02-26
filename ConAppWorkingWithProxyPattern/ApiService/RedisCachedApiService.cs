046016e9c450f6cb562cec6e89752089efcb0470
Kris <kris_s2@outlook.com>
2025-02-25 22:29:12 -0800

    kata: adding read-only dependencies;

--- ConAppWorkingWithProxyPattern/ApiService/RedisCachedApiService.cs
+++ ConAppWorkingWithProxyPattern/ApiService/RedisCachedApiService.cs
@@ -1,4 +1,5 @@
 ﻿using ConAppPlayingWithProxyPattern.Interfaces;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,13 @@ using System.Threading.Tasks;
 namespace ConAppPlayingWithProxyPattern.ApiService;
 public class RedisCachedApiService : IApiService
 {
+	// Implementing the Proxy with Redis.
+	private readonly RealApiService _realApiService;
+	private readonly IDatabase _redisDatabase;
+	private readonly TimeSpan _cacheDuration = TimeSpan.FromSeconds(30);
 
-
+	public Task<string> GetDataAsync(string url)
+	{
+		throw new NotImplementedException();
+	}
 }
