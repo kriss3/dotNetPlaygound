0e2f638e5018e18c4b5ed24cbe518c6f4382fa57
Kris <kris_s2@outlook.com>
2025-02-26 22:19:35 -0800

    kata: Adding a PaymentContext class which servs as an input door to different type of Payment strategies. It uses method injection via void method and priviate variable;

---
+++ src/ConAppPlayingWithStrategyPattern/PaymentContext.cs
@@ -0,0 +1,23 @@
+﻿using ConAppPlayingWithStrategyPattern.Interfaces;
+using static System.Console;
+
+namespace ConAppPlayingWithStrategyPattern;
+public class PaymentContext
+{
+	private IPaymentStrategy? _payStrategy;
+
+	public void SetPaymentStrategy(IPaymentStrategy paymentStrategy) 
+	{
+		_payStrategy = paymentStrategy;
+	}
+
+	public void ProcessPayment(decimal amount) 
+	{
+		if (_payStrategy is null)
+		{
+			WriteLine("Select payment method first!");
+			return;
+		}
+		_payStrategy.Pay(amount);
+	}
+}
