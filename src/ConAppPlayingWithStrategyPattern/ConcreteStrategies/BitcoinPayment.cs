4d6af38451f0d7a9131ec916b5702dbd763d9ef0
Kris <kris_s2@outlook.com>
2025-02-26 22:07:02 -0800

    kata: refactoring, minor changes to BitcoinPayment.

--- src/ConAppPlayingWithStrategyPattern/ConcreteStrategies/BitcoinPayment.cs
+++ src/ConAppPlayingWithStrategyPattern/ConcreteStrategies/BitcoinPayment.cs
@@ -1,12 +1,8 @@
 ﻿using ConAppPlayingWithStrategyPattern.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConAppPlayingWithStrategyPattern.ConcreteStrategies;
-public class CreditCardPayment : IPaymentStrategy
+
+public class BitcoinPayment : IPaymentStrategy
 {
 	public void Pay(decimal amount)
 	{
