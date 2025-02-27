fade93b77d6adc7b31308f1dd2a72bda0083d013
Kris <kris_s2@outlook.com>
2025-02-26 21:59:04 -0800

    kata: moving project PlayingWithProxyPattern under src. Adding PlayingWithStrategyPattern into the solution.

---
+++ src/ConAppPlayingWithStrategyPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using static System.Console;
+
+namespace ConAppPlayingWithStrategyPattern;
+
+internal class Program
+{
+	static void Main()
+	{
+		WriteLine("Playing with the Strategy Pattern!");
+	}
+}
