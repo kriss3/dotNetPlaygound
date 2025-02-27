f3d9cf9a69205c45447c3b563bc70cd14256b8c7
Kris <kris_s2@outlook.com>
2025-02-26 22:05:14 -0800

    kata: scaffolding for different types of payment, different strategies.

---
+++ src/ConAppPlayingWithStrategyPattern/ConcreteStrategies/CreditCardPayment.cs
@@ -0,0 +1,31 @@
+﻿using ConAppPlayingWithStrategyPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppPlayingWithStrategyPattern.ConcreteStrategies;
+public class CreditCardPayment : IPaymentStrategy
+{
+	public void Pay(decimal amount)
+	{
+		throw new NotImplementedException();
+	}
+}
+
+public class PayPalPayment : IPaymentStrategy
+{
+	public void Pay(decimal amount)
+	{
+		throw new NotImplementedException();
+	}
+}
+
+public class BitcoinPayment : IPaymentStrategy
+{
+	public void Pay(decimal amount)
+	{
+		throw new NotImplementedException();
+	}
+}
