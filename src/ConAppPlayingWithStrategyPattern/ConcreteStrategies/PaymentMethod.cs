b574a182b93e169332d82e2c2292078e9dc09cce
Kris <kris_s2@outlook.com>
2025-02-27 23:10:48 -0800

    kata: preparing to use union type in C#

---
+++ src/ConAppPlayingWithStrategyPattern/ConcreteStrategies/PaymentMethod.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppPlayingWithStrategyPattern.ConcreteStrategies;
+internal class PaymentMethod
+{
+}
