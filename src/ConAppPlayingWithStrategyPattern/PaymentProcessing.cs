f5cfd5f866aa85565587371b83cd906b6cbaf1fe
Kris <kris_s2@outlook.com>
2025-02-27 23:28:03 -0800

    kata: adding implementation based on a payment type and an amount.

--- src/ConAppPlayingWithStrategyPattern/PaymentProcessing.cs
+++ src/ConAppPlayingWithStrategyPattern/PaymentProcessing.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using ConAppPlayingWithStrategyPattern.ConcreteStrategies;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using static System.Console;
+
 namespace ConAppPlayingWithStrategyPattern;
-internal class PaymentProcessing
+public class PaymentProcessing
 {
+	public static void ProcessPayment(PaymentMethod method, decimal amount) 
+	{
+		string message = method switch
+		{
+			CreditCard => $"Paid {amount:C} using Credit Card.",
+			PayPal => $"Paid {amount:C} using PayPal payment option.",
+			Bitcoin => $"Paid {amount:C} using Bitcoin payment.",
+			_ => "Invalid choice."
+		};
+
+		WriteLine();
+	}
 }
