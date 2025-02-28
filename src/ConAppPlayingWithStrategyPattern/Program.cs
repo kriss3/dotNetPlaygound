e0bdcb4ca8bfa9be87464d66cae03e5a7195469c
Kris <kris_s2@outlook.com>
2025-02-27 22:46:13 -0800

    kata: begining orchestrating strategy input.

--- src/ConAppPlayingWithStrategyPattern/Program.cs
+++ src/ConAppPlayingWithStrategyPattern/Program.cs
@@ -8,5 +8,17 @@ internal class Program
 	static void Main()
 	{
 		WriteLine("Playing with the Strategy Pattern!");
+		Run();
+	}
+
+	private static void Run() 
+	{
+		var paymentContext = new PaymentContext();
+
+		WriteLine("Select a payment method: 1. Credit Card  2. PayPal  3. Bitcoin");
+		string choice = ReadLine();
+
+
+
 	}
 }
