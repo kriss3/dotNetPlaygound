d22deaedfe3e11b0a6f660eb001af387b3654bce
Kris <kris_s2@outlook.com>
2025-04-25 22:34:38 -0700

    kata: cleanup

--- src/ConAppFunctional/ModelTypes/ResponseWithDetails.cs
+++ src/ConAppFunctional/ModelTypes/ResponseWithDetails.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
+﻿using System.Net;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace ConAppFunctional.ModelTypes;
 public record ResponseWithDetails<TSuccess, TFailure>(
