ba292e6e59a52b1272dd4e04232d4cc62eb2a594
Kris <kris_s2@outlook.com>
2025-04-25 22:31:12 -0700

    kata: working on complex types and functional paradigm.

---
+++ src/ConAppFunctional/ModelTypes/ResponseWithDetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppFunctional.ModelTypes;
+public record ResponseWithDetails<TSuccess, TFailure>(
+		bool IsSuccess, HttpStatusCode Code, string Message, TSuccess Success, TFailure Failure)
+	where TSuccess : class
+	where TFailure : class;
