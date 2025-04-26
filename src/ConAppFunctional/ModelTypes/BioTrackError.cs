ed68cf21721029ebc685aad622c13aace25a5ea6
Kris <kris_s2@outlook.com>
2025-04-25 22:37:49 -0700

    kata: refactor types.

---
+++ src/ConAppFunctional/ModelTypes/BioTrackError.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ConAppFunctional.ModelTypes;
+
+public record BioTrackError
+{
+	[JsonPropertyName("Code")]
+	public string? Code { get; set; }
+	
+	[JsonPropertyName("Data")]
+	public string? Data { get; set; }
+	
+	[JsonPropertyName("Error")]
+	public string? Error { get; set; }
+	
+	[JsonPropertyName("ErrorResource")]
+	public string? ErrorResource { get; set; }
+	
+	[JsonPropertyName("ErrorResourceID")]
+	public string? ErrorResourceId { get; set; }
+	
+	[JsonPropertyName("ErrorSrc")]
+	public string? ErrorSrc { get; set; }
+}
--- src/ConAppFunctional/ModelTypes/ResponseWithDetails.cs
+++ src/ConAppFunctional/ModelTypes/ResponseWithDetails.cs
@@ -1,30 +1,8 @@
 ﻿using System.Net;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace ConAppFunctional.ModelTypes;
 public record ResponseWithDetails<TSuccess, TFailure>(
 		bool IsSuccess, HttpStatusCode Code, string Message, TSuccess Success, TFailure Failure)
 	where TSuccess : class
 	where TFailure : class;
-
-public record BioTrackError
-{
-	[JsonPropertyName("Code")]
-	public string? Code { get; set; }
-	
-	[JsonPropertyName("Data")]
-	public string? Data { get; set; }
-	
-	[JsonPropertyName("Error")]
-	public string? Error { get; set; }
-	
-	[JsonPropertyName("ErrorResource")]
-	public string? ErrorResource { get; set; }
-	
-	[JsonPropertyName("ErrorResourceID")]
-	public string? ErrorResourceId { get; set; }
-	
-	[JsonPropertyName("ErrorSrc")]
-	public string? ErrorSrc { get; set; }
-}
