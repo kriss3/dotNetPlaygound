259ae02b3ab807176496a303e8438c7313d72a2a
Kris <kris_s2@outlook.com>
2025-04-26 22:01:37 -0700

    kata: movie concrete types to its new destination.

---
+++ src/ConAppFunctional/ModelTypes/ConcreteServiceError.cs
@@ -0,0 +1,24 @@
+﻿using Cova.ServiceErrors.Errors;
+
+namespace ConAppFunctional.ModelTypes;
+
+public class ConcreteServiceError(string code, string message) : 
+	ServiceError(code, new Exception(message))
+{
+	public override string ErrorType { get; } = "ConcreteServiceError";
+	public override IEnumerable<ErrorCode> ErrorCodes { get; } = [];
+
+	public override T Match<T>(
+		Func<PersistenceDataError, T> persistenceDataError,
+		Func<UnexpectedError, T> unexpectedError,
+		Func<ValidationError, T> validationError,
+		Func<MappingError, T> mappingError,
+		Func<AuthorizationError, T> authorizationError,
+		Func<AuthenticationError, T> authenticationError,
+		Func<CustomError, T> customError)
+	{
+		// Implement the match logic here
+		throw new NotImplementedException();
+	}
+
+}
