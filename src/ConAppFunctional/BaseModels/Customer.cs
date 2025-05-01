72f298a7b93cd0cd7c9a91aa6b027f45413d022b
Kris <kris_s2@outlook.com>
2025-04-30 22:40:24 -0700

    kata: adding Customer model

---
+++ src/ConAppFunctional/BaseModels/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppFunctional.BaseModels;
+public class Customer
+{
+	public int Id { get; set; }
+	public string? FirstName { get; set; }
+	public string? LastName { get; set; }
+}
