cf1ddf2e4dc4b50c1ea0678303f201f3eef2d87f
Kris <kris_s2@outlook.com>
2025-05-01 22:32:22 -0700

    kata: cleaner return;

--- src/ConAppFunctional/Helpers/FunctionalHelpers.cs
+++ src/ConAppFunctional/Helpers/FunctionalHelpers.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net;
 
 namespace ConAppFunctional.Helpers;
 public static class FunctionalHelpers
@@ -15,9 +10,7 @@ public static class FunctionalHelpers
 			Content = new StringContent("{\"CustomerId\": 1, \"Name\": \"John Doe\"}", System.Text.Encoding.UTF8, "application/json")
 		};
 
-		// Simulate the behavior of the actual API call
-		await Task.Delay(100); // Simulate network delay
-		var response = fakeResponse;
-		return response;
+		await Task.Delay(100);
+		return fakeResponse;
 	}
 }
