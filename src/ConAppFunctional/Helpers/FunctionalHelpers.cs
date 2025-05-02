af3871272bada241083061ab5dea1bedcbc67b85
Kris <kris_s2@outlook.com>
2025-05-01 22:30:37 -0700

    kata: stabing a static function to fake http call.

---
+++ src/ConAppFunctional/Helpers/FunctionalHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppFunctional.Helpers;
+public static class FunctionalHelpers
+{
+	public static HttpResponseMessage GetFakeHttpData() 
+	{
+	
+	}
+}
