d799034af8105050c6ad9e79614586ce449ecf57
Kris <kris_s2@outlook.com>
2025-05-04 21:34:13 -0700

    kata: moving User type to its own file

--- src/ConAppFunctional/Helpers/FunctionalHelpers.cs
+++ src/ConAppFunctional/Helpers/FunctionalHelpers.cs
@@ -155,9 +155,3 @@ public class UserProfile
 	}
 }
 
-public class User(int id, string name)
-{
-	public int Id { get; } = id;
-	public string Name { get; } = name;
-}
-
---
+++ src/ConAppFunctional/Helpers/User.cs
@@ -0,0 +1,8 @@
+﻿namespace ConAppFunctional.Helpers;
+
+public class User(int id, string name)
+{
+	public int Id { get; } = id;
+	public string Name { get; } = name;
+}
+
