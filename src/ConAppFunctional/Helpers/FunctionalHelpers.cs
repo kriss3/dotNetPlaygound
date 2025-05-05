24785710965206e52631456017c55a1463fca6e4
Kris <kris_s2@outlook.com>
2025-05-04 21:29:11 -0700

    kata: working on another mutable/immutable type

--- src/ConAppFunctional/Helpers/FunctionalHelpers.cs
+++ src/ConAppFunctional/Helpers/FunctionalHelpers.cs
@@ -143,3 +143,14 @@ public class ImmutableCustomer
 		repo.SaveCustomer(customer);
 	}
 }
+
+
+public class UserProfile
+{
+	private User _user;
+	private string _address;
+	public void UpdateUser(int userId, string name)
+	{
+		_user = new User(userId, name);
+	}
+}
