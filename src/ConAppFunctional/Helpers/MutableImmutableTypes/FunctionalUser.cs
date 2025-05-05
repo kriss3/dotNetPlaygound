efc02e09c982d3bf13f47eb2cd81799912b52336
Kris <kris_s2@outlook.com>
2025-05-04 21:40:57 -0700

    kata: cleanup. It is a UserProfile that needs fixing

--- src/ConAppFunctional/Helpers/MutableImmutableTypes/FunctionalUser.cs
+++ src/ConAppFunctional/Helpers/MutableImmutableTypes/FunctionalUser.cs
@@ -5,16 +5,3 @@ public class FunctionalUser(int id, string name)
 	public int Id { get; } = id;
 	public string Name { get; } = name;
 }
-
-public class FunctionalUser_v2
-{
-	public int Id { get; }
-	public string Name { get; }
-	public FunctionalUser_v2(int id, string name)
-	{
-		Id = id;
-		Name = name;
-	}
-}
-
-
