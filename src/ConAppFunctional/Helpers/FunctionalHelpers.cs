7a0fc4804f38ad2690c4181846af2face19721bd
Kris <kris_s2@outlook.com>
2025-05-04 21:34:39 -0700

    kata: moving UserProfile to its own class

--- src/ConAppFunctional/Helpers/FunctionalHelpers.cs
+++ src/ConAppFunctional/Helpers/FunctionalHelpers.cs
@@ -144,14 +144,3 @@ public class ImmutableCustomer
 	}
 }
 
-
-public class UserProfile
-{
-	private User? _user;
-	private string? _address;
-	public void UpdateUser(int userId, string name)
-	{
-		_user = new User(userId, name);
-	}
-}
-
---
+++ src/ConAppFunctional/Helpers/UserProfile.cs
@@ -0,0 +1,12 @@
+﻿namespace ConAppFunctional.Helpers;
+
+public class UserProfile
+{
+	private User? _user;
+	private string? _address;
+	public void UpdateUser(int userId, string name)
+	{
+		_user = new User(userId, name);
+	}
+}
+
