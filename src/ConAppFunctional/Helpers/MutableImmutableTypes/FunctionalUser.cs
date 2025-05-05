3792cc4865a494234898233e30ef975c601d9d58
Kris <kris_s2@outlook.com>
2025-05-04 21:39:58 -0700

    kata: alternative implementation of FunctionalUser wihtout the state.

--- src/ConAppFunctional/Helpers/MutableImmutableTypes/FunctionalUser.cs
+++ src/ConAppFunctional/Helpers/MutableImmutableTypes/FunctionalUser.cs
@@ -6,3 +6,15 @@ public class FunctionalUser(int id, string name)
 	public string Name { get; } = name;
 }
 
+public class FunctionalUser_v2
+{
+	public int Id { get; }
+	public string Name { get; }
+	public FunctionalUser_v2(int id, string name)
+	{
+		Id = id;
+		Name = name;
+	}
+}
+
+
