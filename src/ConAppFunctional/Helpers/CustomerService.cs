c7d9ac1b65e271b71976fdbf78e5c3deac4df722
Kris <kris_s2@outlook.com>
2025-05-05 22:43:49 -0700

    kata: adding descriptors to the steps names: CQS style.

--- src/ConAppFunctional/Helpers/CustomerService.cs
+++ src/ConAppFunctional/Helpers/CustomerService.cs
@@ -2,22 +2,31 @@
 
 namespace ConAppFunctional.Helpers;
 
+
+// This is a typical class with side effects.
 public class CustomerService
 {
 	public void Process(string customerName, string addressString)
 	{
+		//Command
 		Address address = CreateAddress(addressString);
 		Customer customer = CreateCustomer(customerName, address);
 		SaveCustomer(customer);
 	}
+
+	//Query
 	private Address CreateAddress(string addressString)
 	{
 		return new Address(addressString);
 	}
+
+	//Query
 	private Customer CreateCustomer(string name, Address address)
 	{
 		return new Customer(name, address);
 	}
+
+	//Command
 	private void SaveCustomer(Customer customer)
 	{
 		var repository = new CustomerRepository();
@@ -25,3 +34,4 @@ public class CustomerService
 	}
 
 }
+
