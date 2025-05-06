15ba92ccc299dc2af69c92d266a537412a4508fc
Kris <kris_s2@outlook.com>
2025-05-05 22:37:39 -0700

    kata: renaming class for better reference.

---
+++ src/ConAppFunctional/Helpers/CustomerService.cs
@@ -0,0 +1,32 @@
+﻿using ConAppFunctional.BaseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppFunctional.Helpers;
+
+public class CustomerService
+{
+	public void Process(string customerName, string addressString)
+	{
+		Address address = CreateAddress(addressString);
+		Customer customer = CreateCustomer(customerName, address);
+		SaveCustomer(customer);
+	}
+	private Address CreateAddress(string addressString)
+	{
+		return new Address(addressString);
+	}
+	private Customer CreateCustomer(string name, Address address)
+	{
+		return new Customer(name, address);
+	}
+	private void SaveCustomer(Customer customer)
+	{
+		var repository = new Repository();
+		repository.Save(customer);
+	}
+
+}
