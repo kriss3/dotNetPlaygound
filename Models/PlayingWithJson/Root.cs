62e577537f541d04682abbfa9d7c3fd98072132e
Kris <kris_s2@outlook.com>
2025-05-09 23:10:33 -0700

    kata: adding model fixes.

--- Models/PlayingWithJson/Root.cs
+++ Models/PlayingWithJson/Root.cs
@@ -1,25 +1,27 @@
 ﻿using System.Text.Json.Serialization;
-
 namespace Models.PlayingWithJson;
 
 public class Root
 {
-	public Invoice Invoice { get; set; }
+	public Invoice? Invoice { get; set; }
 }
 
 public class Invoice
 {
 	[JsonPropertyName("InvoiceID")]
 	public string? InvoiceId { get; set; }
-	public List<InventoryItem> Inventory { get; set; }
-	public List<PaymentItem> Payment { get; set; }
+	public List<InventoryItem>? Inventory { get; set; }
+	public List<PaymentItem>? Payment { get; set; }
 }
 
 public class InventoryItem
 {
-	public int ID { get; set; }
-	public string InvoiceID { get; set; }
-	public string InventoryID { get; set; }
+	[JsonPropertyName("ID")]
+	public int Id { get; set; }
+	[JsonPropertyName("InvoiceID")]
+	public string? InvoiceId { get; set; }
+	[JsonPropertyName("InventoryID")]
+	public string? InventoryId { get; set; }
 	public bool Deleted { get; set; }
 	public decimal Amount { get; set; }
 	public decimal Price { get; set; }
@@ -28,11 +30,13 @@ public class InventoryItem
 
 public class PaymentItem
 {
-	public int ID { get; set; }
+	[JsonPropertyName("Id")]
+	public int PaymentItemId { get; set; }
 	public decimal Amount { get; set; }
-	public string InvoiceID { get; set; }
+	[JsonPropertyName("InvoiceID")]
+	public string? InvoiceId { get; set; }
 	public bool Deleted { get; set; }
 	public int TransactionID { get; set; }
-	public string CreatedOn { get; set; }
-	public string UpdatedOn { get; set; }
+	public string? CreatedOn { get; set; }
+	public string? UpdatedOn { get; set; }
 }
