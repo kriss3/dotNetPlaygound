8566bc4cfd518e4796e1e9cab58d26050a15c95e
Kris <kris_s2@outlook.com>
2025-05-09 23:03:56 -0700

    kata: adding Invoice model.

--- Models/PlayingWithJson/Root.cs
+++ Models/PlayingWithJson/Root.cs
@@ -4,3 +4,10 @@ public class Root
 {
 	public Invoice Invoice { get; set; }
 }
+
+public class Invoice
+{
+	public string InvoiceID { get; set; }
+	public List<InventoryItem> Inventory { get; set; }
+	public List<PaymentItem> Payment { get; set; }
+}
