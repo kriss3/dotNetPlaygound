15767354732a55e7aeb32cb384ec520bb8d0a110
Kris <kris_s2@outlook.com>
2025-05-09 23:04:42 -0700

    kata: fixing props.

--- Models/PlayingWithJson/Root.cs
+++ Models/PlayingWithJson/Root.cs
@@ -1,4 +1,6 @@
-﻿namespace Models.PlayingWithJson;
+﻿using System.Text.Json.Serialization;
+
+namespace Models.PlayingWithJson;
 
 public class Root
 {
@@ -7,7 +9,8 @@ public class Root
 
 public class Invoice
 {
-	public string InvoiceID { get; set; }
+	[JsonPropertyName("InvoiceID")]
+	public string? InvoiceId { get; set; }
 	public List<InventoryItem> Inventory { get; set; }
 	public List<PaymentItem> Payment { get; set; }
 }
