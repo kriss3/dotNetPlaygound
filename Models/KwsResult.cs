d62ffcb80d05cbbfab184c8d4be95695827900fa
Kris <kris_s2@outlook.com>
2025-05-10 12:00:42 -0700

    kata: attempt to create my own version of Result type.

---
+++ Models/KwsResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models;
+internal class KwsResult
+{
+	public bool IsSuccess { get; }
+	public string? Error { get; private set; }
+}
