54b0827e1768a5a5f6fa8c505d44f0e95a475008
Kris <kris_s2@outlook.com>
2025-05-11 23:22:03 -0700

    kata: introducing an alternative to KwsResult type with enum instead of strings.

--- Models/KwsResult.cs
+++ Models/KwsResult.cs
@@ -38,6 +38,47 @@ public class KwsResult
 }
 
 
+
+public class KwsResult_v2
+{
+	public bool IsSuccess { get; }
+	public string? Error { get; private set; }
+	public bool IsFailure => !IsSuccess;
+
+	protected KwsResult(bool isSuccess, string error)
+	{
+		if (isSuccess && error != string.Empty)
+			throw new InvalidOperationException();
+		if (!isSuccess && error == string.Empty)
+			throw new InvalidOperationException();
+
+		IsSuccess = isSuccess;
+		Error = error;
+	}
+
+	public static KwsResult Fail(string message)
+	{
+		return new KwsResult(false, message);
+	}
+
+	public static KwsResult<T> Fail<T>(string message)
+	{
+		return new KwsResult<T>(default, false, message);
+	}
+
+	public static KwsResult Ok()
+	{
+		return new KwsResult(true, string.Empty);
+	}
+
+	public static KwsResult<T> Ok<T>(T value)
+	{
+		return new KwsResult<T>(value, true, string.Empty);
+	}
+}
+
+
+
 public class KwsResult<T> : KwsResult 
 {
 	public T? Value { get; }
