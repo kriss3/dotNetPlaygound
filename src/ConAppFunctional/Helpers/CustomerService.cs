911778b037cf75a8cc3db4faf06ba73d2defe2c6
Kris <kris_s2@outlook.com>
2025-05-13 22:30:14 -0700

    kata: reset and starting agin with Result.

--- src/ConAppFunctional/Helpers/CustomerService.cs
+++ src/ConAppFunctional/Helpers/CustomerService.cs
@@ -1,4 +1,5 @@
 ﻿using ConAppFunctional.BaseModels;
+using Cova.Functional;
 using Models;
 
 namespace ConAppFunctional.Helpers;
@@ -35,40 +36,3 @@ public class CustomerService
 	}
 
 }
-
-public class CustomerService_v2 
-{
-
-	// this is the client of the KwsResult
-	private static void CreateCustomer(string name, Address address)
-	{
-		var customer = new Customer(name, address);
-		KwsResult<Customer> result = SaveCustomer(customer);
-
-		if (result.IsFailure) {
-			Console.WriteLine("Error connecting to the Db. Please try again later.");
-		}
-	}
-
-	private static KwsResult<Customer> SaveCustomer(Customer customer) 
-	{
-
-		return new KwsResult<Customer>(customer, false, "");
-	}
-
-	private static KwsResult<Customer>? GetCustomer(int id) 
-	{
-		try
-		{
-			var ctx = new List<Customer>();
-			return KwsResult.Ok(ctx.Single(x => x.Id == id));
-		}
-		catch (Exception ex)
-		{
-			if(ex.Message == "Unable to open the Db connection.")
-				return KwsResult.Fail<Customer>(ex.Message);
-			throw;
-		}
-	}
-}
-
