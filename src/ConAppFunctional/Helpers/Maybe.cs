452cec17037fea7aebe70375f4123331cf71e994
Kris <kris_s2@outlook.com>
2025-06-24 23:46:13 -0700

    kata: adding getter implementiona and public method to check for value status.

--- src/ConAppFunctional/Helpers/Maybe.cs
+++ src/ConAppFunctional/Helpers/Maybe.cs
@@ -8,6 +8,22 @@ namespace ConAppFunctional.Helpers;
 public class Maybe<T> : IEquatable<Maybe<T>>
 	where T : class
 {
+	private readonly T _value;
+	public T Value 
+	{
+		get 
+		{
+			if (HasNoValue)
+				throw new InvalidOperationException();
+
+			return _value;
+		}
+	}
+
+	public bool HasValue => _value != null;
+	public bool HasNoValue => !HasValue;
+
+
 	public bool Equals(Maybe<T>? other)
 	{
 		throw new NotImplementedException();
