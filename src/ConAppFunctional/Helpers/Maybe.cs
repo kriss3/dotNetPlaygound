ecd35b1abb65a200a793ad5af98ab4fecddce40e
Kris <kris_s2@outlook.com>
2025-06-28 16:24:08 -0700

    kata: cleanup.

--- src/ConAppFunctional/Helpers/Maybe.cs
+++ src/ConAppFunctional/Helpers/Maybe.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConAppFunctional.Helpers;
+﻿namespace ConAppFunctional.Helpers;
 public class Maybe<T> : IEquatable<Maybe<T>>
 				where T : class
 {
