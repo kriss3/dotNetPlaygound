66946fdfa7b956a7a32af5d769f3e55c4e114355
Kris <kris_s2@outlook.com>
2025-06-28 16:23:55 -0700

    kata: fixing nullable value in Functional Helper

--- src/ConAppFunctional/Helpers/Maybe.cs
+++ src/ConAppFunctional/Helpers/Maybe.cs
@@ -6,26 +6,44 @@ using System.Threading.Tasks;
 
 namespace ConAppFunctional.Helpers;
 public class Maybe<T> : IEquatable<Maybe<T>>
-	where T : class
+				where T : class
 {
-	private readonly T _value;
-	public T Value 
+	private readonly T? _value;
+	public T Value
 	{
-		get 
+		get
 		{
 			if (HasNoValue)
 				throw new InvalidOperationException();
 
-			return _value;
+			return _value!;
 		}
 	}
 
 	public bool HasValue => _value != null;
 	public bool HasNoValue => !HasValue;
 
-
 	public bool Equals(Maybe<T>? other)
 	{
-		throw new NotImplementedException();
+		if (other is null)
+			return false;
+
+		if (ReferenceEquals(this, other))
+			return true;
+
+		return EqualityComparer<T>.Default.Equals(_value, other._value);
+	}
+
+	public override bool Equals(object? obj)
+	{
+		if (obj is Maybe<T> other)
+			return Equals(other);
+
+		return false;
+	}
+
+	public override int GetHashCode()
+	{
+		return _value?.GetHashCode() ?? 0;
 	}
 }
