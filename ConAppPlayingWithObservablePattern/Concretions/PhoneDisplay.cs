5e14af8d5c240b7dfd8a67cbf72f66c565fcbc07
Kris <kris_s2@outlook.com>
2025-06-28 17:25:10 -0700

    kata: adding scafolding of WeatherStation and PhoneDisplay. Adding relationships and scafolding of the required functions.

--- ConAppPlayingWithObservablePattern/Concretions/PhoneDisplay.cs
+++ ConAppPlayingWithObservablePattern/Concretions/PhoneDisplay.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using ConAppPlayingWithObservablePattern.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConAppPlayingWithObservablePattern.Concretions;
-internal class PhoneDisplay
+public class PhoneDisplay : IObserver
 {
+	public Task Update()
+	{
+		throw new NotImplementedException();
+	}
 }
--- ConAppPlayingWithObservablePattern/Concretions/WeatherStation.cs
+++ ConAppPlayingWithObservablePattern/Concretions/WeatherStation.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using ConAppPlayingWithObservablePattern.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConAppPlayingWithObservablePattern.Concretions;
-internal class WeatherStation
+public class WeatherStation : IObservable
 {
+	public Task Add(IObserver observer)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task Notify()
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task Remove(IObserver observer)
+	{
+		throw new NotImplementedException();
+	}
 }
