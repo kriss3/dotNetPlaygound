8ddd20a0b072121036197b0fe4da5975fd0a4877
Kris <kris_s2@outlook.com>
2025-06-28 17:34:12 -0700

    kata: working on WeatherStation (Obserbable => subject). Adding all methods and implementations.

--- ConAppPlayingWithObservablePattern/Concretions/WeatherStation.cs
+++ ConAppPlayingWithObservablePattern/Concretions/WeatherStation.cs
@@ -8,18 +8,30 @@ using System.Threading.Tasks;
 namespace ConAppPlayingWithObservablePattern.Concretions;
 public class WeatherStation : IObservable
 {
+	private readonly IList<IObserver> observers = [];
+	private readonly int _temperature = 15;
+
 	public Task Add(IObserver observer)
 	{
-		throw new NotImplementedException();
-	}
-
-	public Task Notify()
-	{
-		throw new NotImplementedException();
+		observers.Add(observer);
+		return Task.CompletedTask;
 	}
 
 	public Task Remove(IObserver observer)
 	{
-		throw new NotImplementedException();
+		if (observers.Count == 0) return Task.CompletedTask;
+		observers.Remove(observer);
+		return Task.CompletedTask;
+	}
+
+	public Task Notify()
+	{
+		observers.ToList().ForEach(observer => observer.Update());
+		return Task.CompletedTask;
+	}
+
+	public int GetTemperature()
+	{
+		return _temperature;
 	}
 }
