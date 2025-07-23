ab6de65fddc73b6d1e18115579e8425baaaf0736
Kris <kris_s2@outlook.com>
2025-07-22 23:48:08 -0700

    kata: adding topic producer.

---
+++ src/PlayingWithAsb_Part2/Producer/TopicProducer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayingWithAsb_Part2.Producer;
+public class TopicProducer
+{
+	private readonly IConfiguration _config;
+
+	public TopicProducer(IConfiguration config)
+	{
+		_config = config;
+	}
+}
