9273a1b976e633a8683887aeb9a6ccdcd046bbbd
Kris <kris_s2@outlook.com>
2025-07-24 23:39:02 -0700

    kata: cleanup

--- src/PlayingWithAsb_Part2/Consumer/TopicConsumer.cs
+++ src/PlayingWithAsb_Part2/Consumer/TopicConsumer.cs
@@ -1,10 +1,5 @@
 ﻿using Azure.Messaging.ServiceBus;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PlayingWithAsb_Part2.Consumer;
 public class TopicConsumer(IConfiguration config)
