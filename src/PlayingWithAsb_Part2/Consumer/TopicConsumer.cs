338efc00365085b2c1b5964f4c973984fc45b285
Kris <kris_s2@outlook.com>
2025-07-24 23:38:50 -0700

    kata: cleanup

--- src/PlayingWithAsb_Part2/Consumer/TopicConsumer.cs
+++ src/PlayingWithAsb_Part2/Consumer/TopicConsumer.cs
@@ -7,13 +7,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace PlayingWithAsb_Part2.Consumer;
-internal class TopicConsumer
+public class TopicConsumer(IConfiguration config)
 {
-	private readonly IConfiguration _config;
+	private readonly IConfiguration _config = config;
 	private ServiceBusProcessor? _processor;
-
-	public TopicConsumer(IConfiguration config)
-	{
-		_config = config;
-	}
 }
