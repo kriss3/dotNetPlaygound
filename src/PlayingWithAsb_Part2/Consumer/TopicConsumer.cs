a30c5537dcf7101da02b99cdbe3c36e31df57466
Kris <kris_s2@outlook.com>
2025-07-24 23:43:38 -0700

    kata: adding Receiver mechanism.

--- src/PlayingWithAsb_Part2/Consumer/TopicConsumer.cs
+++ src/PlayingWithAsb_Part2/Consumer/TopicConsumer.cs
@@ -6,4 +6,32 @@ public class TopicConsumer(IConfiguration config)
 {
 	private readonly IConfiguration _config = config;
 	private ServiceBusProcessor? _processor;
+
+	public async Task StartReceivingAsync(CancellationToken cancellationToken = default)
+	{
+		var connectionString = _config["ConnectionStrings:ASB_CONN_STRING"];
+		var topicName = _config["TOPIC_NAME"];
+		var subscriptionName = _config["SUBSCRIPTION_NAME"];
+
+		var clientOptions = new ServiceBusClientOptions()
+		{
+			TransportType = ServiceBusTransportType.AmqpWebSockets
+		};
+
+		var client = new ServiceBusClient(connectionString, clientOptions);
+
+		var processorOptions = new ServiceBusProcessorOptions
+		{
+			MaxConcurrentCalls = 1,
+			AutoCompleteMessages = false
+		};
+
+		_processor = client.CreateProcessor(topicName, subscriptionName, processorOptions);
+
+		_processor.ProcessMessageAsync += MessageHandler;
+		_processor.ProcessErrorAsync += ErrorHandler;
+
+		await _processor.StartProcessingAsync(cancellationToken);
+		Console.WriteLine($"Started processing messages from topic '{topicName}', subscription '{subscriptionName}'");
+	}
 }
