70e777c4f24414c170664905fb637dc5c5c8658c
Kris <kris_s2@outlook.com>
2025-07-24 23:32:24 -0700

    kata: adding SendBatchMessage to send a collection of messages to the bus.

--- src/PlayingWithAsb_Part2/Producer/TopicProducer.cs
+++ src/PlayingWithAsb_Part2/Producer/TopicProducer.cs
@@ -38,4 +38,45 @@ public class TopicProducer(IConfiguration config)
 			throw;
 		}
 	}
+
+	public async Task SendBatchMessagesAsync(IEnumerable<string> messages)
+	{
+		var connectionString = _config["ConnectionStrings:ASB_CONN_STRING"];
+		var topicName = _config["TOPIC_NAME"];
+
+		var clientOptions = new ServiceBusClientOptions()
+		{
+			TransportType = ServiceBusTransportType.AmqpWebSockets
+		};
+
+		await using var client = new ServiceBusClient(connectionString, clientOptions);
+		await using var sender = client.CreateSender(topicName);
+
+		using var messageBatch = await sender.CreateMessageBatchAsync();
+
+		foreach (var message in messages)
+		{
+			var serviceBusMessage = new ServiceBusMessage(message)
+			{
+				MessageId = Guid.NewGuid().ToString(),
+				Subject = "Batch Message"
+			};
+
+			if (!messageBatch.TryAddMessage(serviceBusMessage))
+			{
+				throw new Exception($"The message '{message}' is too large to fit in the batch.");
+			}
+		}
+
+		try
+		{
+			await sender.SendMessagesAsync(messageBatch);
+			Console.WriteLine($"Batch of {messageBatch.Count} messages sent to topic '{topicName}'");
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Error sending batch messages: {ex.Message}");
+			throw;
+		}
+	}
 }
