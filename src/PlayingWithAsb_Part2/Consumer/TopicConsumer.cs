28d468d6a2e1cd39e51bf8dc6b2642ed86e049ba
Kris <kris_s2@outlook.com>
2025-07-24 23:38:31 -0700

    kata: beginning of consumer implementation.

--- src/PlayingWithAsb_Part2/Consumer/TopicConsumer.cs
+++ src/PlayingWithAsb_Part2/Consumer/TopicConsumer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,4 +9,11 @@ using System.Threading.Tasks;
 namespace PlayingWithAsb_Part2.Consumer;
 internal class TopicConsumer
 {
+	private readonly IConfiguration _config;
+	private ServiceBusProcessor? _processor;
+
+	public TopicConsumer(IConfiguration config)
+	{
+		_config = config;
+	}
 }
