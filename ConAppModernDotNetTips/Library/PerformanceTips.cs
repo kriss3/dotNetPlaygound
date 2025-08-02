0f0e90cf19741bfba79eb60e3ef5672eef4fe8de
Kris <kris_s2@outlook.com>
2025-08-02 11:25:52 -0700

    kata: working with the presized dictionary.

--- ConAppModernDotNetTips/Library/PerformanceTips.cs
+++ ConAppModernDotNetTips/Library/PerformanceTips.cs
@@ -7,4 +7,14 @@ using System.Threading.Tasks;
 namespace ConAppModernDotNetTips.Library;
 public static class PerformanceTips
 {
+	//Pre-sizing dictionary:
+	public static Dictionary<string, string> CreatePresizedDictionary(IEnumerable<string> keys) 
+	{
+		var dict = new Dictionary<string, string>();
+		foreach (var k in keys) 
+		{
+			dict[k] = $"The Value for {k}";
+		}
+		return dict;
+	}
 }
