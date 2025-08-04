ec57e850abef11157ef5ab0c2fefd7d80629455a
Kris <kris_s2@outlook.com>
2025-08-04 16:52:43 -0700

    kata: attempt to convert this project to a proper console app and try to work with Key Vault.

---
+++ src/ConAppSimpleLib/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace ConAppSimpleLib;
+public class Program
+{
+	public static void Main() 
+	{
+
+	}
+}
