9ef407b915a708604d42dd452e5b447f171c0c06
Kris <kris_s2@outlook.com>
2025-08-04 18:59:48 -0700

    kata: minor cleanup

--- src/ConAppSimpleLib/Infrastructure/AppSettings.cs
+++ src/ConAppSimpleLib/Infrastructure/AppSettings.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConAppSimpleLib.Infrastructure;
+﻿namespace ConAppSimpleLib.Infrastructure;
 public class AppSettings
 {
 	public Configuration? Configuration { get; set; }
