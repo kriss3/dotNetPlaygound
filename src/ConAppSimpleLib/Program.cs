b9d9c93bd35dff80c48b0730a0d1f7de245b97d0
Kris <kris_s2@outlook.com>
2025-08-04 19:04:50 -0700

    kata: attempt to configure access to the key vault

--- src/ConAppSimpleLib/Program.cs
+++ src/ConAppSimpleLib/Program.cs
@@ -1,16 +1,26 @@
 ﻿using Microsoft.Extensions.Configuration;
-
+using System.Threading.Tasks;
 using static System.Console;
 
 namespace ConAppSimpleLib;
 public class Program
 {
-	public static void Main() 
+	public static async Task Main() 
 	{
 		WriteLine("My attempt to work with Az Key Vault.");
 		// Load configuration from appsettings.json
 		var config = new ConfigurationBuilder()
 			.AddJsonFile("appsettings.json")
 			.Build();
+
+		string vaultUrl = config["Configuration:AzureVaultUrl"]!;
+
+		if (string.IsNullOrEmpty(vaultUrl)) 
+		{
+			WriteLine("Vault URL not found in configuration.");
+			return;
+		}
+
+		string? secret = await MyConfig.GetSecretAsync(vaultUrl, "kwsConnString");
 	}
 }
