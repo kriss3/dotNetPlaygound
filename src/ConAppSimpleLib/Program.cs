ce9507d2b00ad78be4eae823b86604b91e81397c
Kris <kris_s2@outlook.com>
2025-08-09 18:26:44 -0700

    kata: minor cleanup

--- src/ConAppSimpleLib/Program.cs
+++ src/ConAppSimpleLib/Program.cs
@@ -20,9 +20,9 @@ public class Program
 			return;
 		}
 
-		// Capture erorr correctly when DefaultAuthentication did not work due to:
+		// Capture errors correctly when DefaultAuthentication did not work due to:
 		/*
-		 * rror retrieving secret: DefaultAzureCredential failed to retrieve a token from the included credentials. See the troubleshooting guide for more information. 
+		 * Error retrieving secret: DefaultAzureCredential failed to retrieve a token from the included credentials. See the troubleshooting guide for more information. 
 		 */
 		string? secret = await MyConfig.GetSecretAsync(vaultUrl, "kwsConnString");
 
