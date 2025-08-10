eecc4b59d45468edb6f39bbcc5d686bd336d96e4
Kris <kris_s2@outlook.com>
2025-08-09 19:29:56 -0700

    kata: adding 5 different functions to the library to be available in other project.

--- AzureKeyVaultHelper/KeyVaultHelper.cs
+++ AzureKeyVaultHelper/KeyVaultHelper.cs
@@ -7,4 +7,14 @@ using System.Threading.Tasks;
 namespace AzureKeyVaultHelper;
 public static class KeyVaultHelper
 {
+	public static async Task<string> GetSecretAsync(string secretName) { }
+
+	public static async Task<string> GetSecretAsync(string vaultUrl, string secretKey) { }
+
+	public static async Task<string?> GetSecretOrNullAsync(string vaultUrl, string secretKey) { }
+
+	public static async Task<bool> SecretExistsAsync(string vaultUrl, string secretKey) { }
+
+	public static async Task<Dictionary<string, string>> GetMultipleSecretsAsync(string vaultUrl, params string[] secretKeys) { }
+
 }
