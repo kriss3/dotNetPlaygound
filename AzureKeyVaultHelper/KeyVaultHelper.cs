c26ede6232802d49552192f2c1d869d90e4a62b0
Kris <kris_s2@outlook.com>
2025-08-09 19:36:49 -0700

    kata: adding implementation of the first function. This one with hardocded vault url.

--- AzureKeyVaultHelper/KeyVaultHelper.cs
+++ AzureKeyVaultHelper/KeyVaultHelper.cs
@@ -7,14 +7,34 @@ using System.Threading.Tasks;
 namespace AzureKeyVaultHelper;
 public static class KeyVaultHelper
 {
-	public static async Task<string> GetSecretAsync(string secretName) { }
+	/// <summary>
+	/// Retrieves a secret value from Azure Key Vault using a hardcoded vault URL
+	/// </summary>
+	/// <param name="secretName">The name of the secret to retrieve</param>
+	/// <returns>The secret value, or empty string if error occurred</returns>
+	public static async Task<string> GetSecretAsync(string secretName) 
+	{
+		return await GetSecretAsync("https://mykeyvault2021.vault.azure.net/", secretName);
+	}
 
-	public static async Task<string> GetSecretAsync(string vaultUrl, string secretKey) { }
+	public static async Task<string> GetSecretAsync(string vaultUrl, string secretKey) 
+	{
 
-	public static async Task<string?> GetSecretOrNullAsync(string vaultUrl, string secretKey) { }
+	}
 
-	public static async Task<bool> SecretExistsAsync(string vaultUrl, string secretKey) { }
+	public static async Task<string?> GetSecretOrNullAsync(string vaultUrl, string secretKey) 
+	{
+	
+	}
 
-	public static async Task<Dictionary<string, string>> GetMultipleSecretsAsync(string vaultUrl, params string[] secretKeys) { }
+	public static async Task<bool> SecretExistsAsync(string vaultUrl, string secretKey) 
+	{
+	
+	}
+
+	public static async Task<Dictionary<string, string>> GetMultipleSecretsAsync(string vaultUrl, params string[] secretKeys) 
+	{
+	
+	}
 
 }
