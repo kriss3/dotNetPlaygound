86ffa3b487bdcdef23d736191e0755758e35124e
Kris <kris_s2@outlook.com>
2025-08-12 23:06:52 -0700

    kata: cleanup

--- LargePrSimulation.Library/MathService.cs
+++ LargePrSimulation.Library/MathService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LargePrSimulation.Library;
+﻿namespace LargePrSimulation.Library;
 public class MathService
 {
 	public int Add(int x, int y) => x + y;
