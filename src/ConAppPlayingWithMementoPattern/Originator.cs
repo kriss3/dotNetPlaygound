1f0fc3a8dd300f13f3d76d2c142d07938f472c36
Kris <kris_s2@outlook.com>
2025-08-17 19:12:30 -0700

    kata: working with originator.

--- src/ConAppPlayingWithMementoPattern/Originator.cs
+++ src/ConAppPlayingWithMementoPattern/Originator.cs
@@ -7,4 +7,15 @@ using System.Threading.Tasks;
 namespace ConAppPlayingWithMementoPattern;
 public class Originator
 {
+	private string? _state;
+
+	public string? State 
+	{
+		get { return _state; }
+		set 
+		{
+			_state = value;
+			Console.WriteLine($"State = {_state}");
+		}
+	}
 }
