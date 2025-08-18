ef75f14299eacacc5a39f92dc28d20e9e6b44e4b
Kris <kris_s2@outlook.com>
2025-08-17 19:15:38 -0700

    kata: cleanup

--- src/ConAppPlayingWithMementoPattern/Originator.cs
+++ src/ConAppPlayingWithMementoPattern/Originator.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConAppPlayingWithMementoPattern;
+﻿namespace ConAppPlayingWithMementoPattern;
 public class Originator
 {
 	private string? _state;
