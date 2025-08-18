0eab954c475a206d888ad7d9cba48b576d241ada
Kris <kris_s2@outlook.com>
2025-08-17 18:54:19 -0700

    kata: adding some docs for learning memento design pattern.

---
+++ src/ConAppPlayingWithMementoPattern/MementoReadme.md
@@ -0,0 +1,3 @@
+﻿# Pattern Descriptin
+Name: Memnto Desing Pattern
+
--- src/ConAppPlayingWithMementoPattern/Program.cs
+++ src/ConAppPlayingWithMementoPattern/Program.cs
@@ -3,7 +3,7 @@ using static System.Console;
 
 namespace ConAppPlayingWithMementoPattern;
 
-internal class Program
+public class Program
 {
 	static Task Main()
 	{
