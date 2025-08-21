7fa2519783b8a7077d81123d9bbb557d298ec33f
Kris <kris_s2@outlook.com>
2025-08-20 22:37:30 -0700

    kata: attempt to scaffold pattern participants.

--- src/ConAppPlayingWithAdapterPattern/Program.cs
+++ src/ConAppPlayingWithAdapterPattern/Program.cs
@@ -9,4 +9,22 @@ public class Program
 		WriteLine("Hello, World!");
 		return Task.CompletedTask;
 	}
+
+	private static void Execute() 
+	{
+		Target target = new Adapter();
+		target.Request();
+		// Wait for user
+		ReadKey();
+	}
+}
+
+public abstract class Target 
+{
+	public virtual void Request() { }
+}
+
+public class Adapter : Target 
+{
+	public override void Request() { }
 }
