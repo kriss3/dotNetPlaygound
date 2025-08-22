56c25f114195b2321fb9f0f23d01766eabe20109
Kris <kris_s2@outlook.com>
2025-08-21 19:26:01 -0700

    kata: attempt to add LegacyGateway to the Example 2.

---
+++ src/ConAppPlayingWithAdapterPattern/Example_2/LegacyGateway.cs
@@ -0,0 +1,8 @@
+﻿namespace ConAppPlayingWithAdapterPattern.Example_2;
+
+
+// Adaptee (3rd party / legacy)
+public sealed class LegacyGateway
+{
+	public string MakePayment(decimal money, string cc) => money > 0 ? "OK" : "ERR";
+}
