c02136047040062a8bf27dd63ffc21e936bf5329
Kris <kris_s2@outlook.com>
2025-08-21 19:22:45 -0700

    kata: attempt to work in Example 2.

---
+++ src/ConAppPlayingWithAdapterPattern/Example_2/IPaymentProcessor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppPlayingWithAdapterPattern.Example_2;
+public interface IPaymentProcessor
+{
+}
