a8f74ee5f32ee188a976259b824f715f456f3810
Kris <kris_s2@outlook.com>
2025-08-22 22:37:32 -0700

    kata: implementing the client for payments using adapter to old way of payments.

--- src/ConAppPlayingWithAdapterPattern/Example_2/CheckoutService.cs
+++ src/ConAppPlayingWithAdapterPattern/Example_2/CheckoutService.cs
@@ -4,4 +4,6 @@
 public sealed class CheckoutService
 {
 	private readonly IPaymentProcessor _payments;
+	public CheckoutService(IPaymentProcessor payments) => _payments = payments;
+	public bool PayOrder(string card, decimal total) => _payments.Charge(card, total);
 }
