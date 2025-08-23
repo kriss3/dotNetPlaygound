ea2b5230825296fb75456610f1afe2ec27ccfe68
Kris <kris_s2@outlook.com>
2025-08-22 22:37:51 -0700

    kata: cleanup

--- src/ConAppPlayingWithAdapterPattern/Example_2/CheckoutService.cs
+++ src/ConAppPlayingWithAdapterPattern/Example_2/CheckoutService.cs
@@ -1,9 +1,9 @@
 ﻿namespace ConAppPlayingWithAdapterPattern.Example_2;
 
 // Client
-public sealed class CheckoutService
+public sealed class CheckoutService(IPaymentProcessor payments)
 {
-	private readonly IPaymentProcessor _payments;
-	public CheckoutService(IPaymentProcessor payments) => _payments = payments;
+	private readonly IPaymentProcessor _payments = payments;
+
 	public bool PayOrder(string card, decimal total) => _payments.Charge(card, total);
 }
