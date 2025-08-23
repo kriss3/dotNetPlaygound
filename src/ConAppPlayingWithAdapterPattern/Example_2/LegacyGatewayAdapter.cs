3941480991e807191db69ae0afb3a787d9b566c5
Kris <kris_s2@outlook.com>
2025-08-22 22:30:34 -0700

    kata: configuring LegacyGatewayAdapter.

--- src/ConAppPlayingWithAdapterPattern/Example_2/LegacyGatewayAdapter.cs
+++ src/ConAppPlayingWithAdapterPattern/Example_2/LegacyGatewayAdapter.cs
@@ -1,8 +1,10 @@
 ﻿namespace ConAppPlayingWithAdapterPattern.Example_2;
 
 // Adapter (Object Adapter)
-public sealed class LegacyGatewayAdapter : IPaymentProcessor
+public sealed class LegacyGatewayAdapter(LegacyGateway legacyGateway) : IPaymentProcessor
 {
+	private readonly LegacyGateway _legacy = legacyGateway;
+
 	public bool Charge(string cardNumber, decimal amount)
 	{
 		throw new NotImplementedException();
