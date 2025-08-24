81b0e696b5f658592af24692ad1ab8f09cd44b0f
Kris <kris_s2@outlook.com>
2025-08-24 11:17:42 -0700

    kata: moving report service.

--- src/ConAppPlayingWithAdapterPattern/Example_3/ILogger.cs
+++ src/ConAppPlayingWithAdapterPattern/Example_3/ILogger.cs
@@ -3,10 +3,3 @@ public interface ILogger
 { 
 	void Log(string message); 
 }
-
-public sealed class ReportService(ILogger logger)
-{
-	private readonly ILogger _logger = logger;
-
-	public void Run() => _logger.Log("Report executed.");
-}
---
+++ src/ConAppPlayingWithAdapterPattern/Example_3/ReportService.cs
@@ -0,0 +1,8 @@
+﻿namespace ConAppPlayingWithAdapterPattern.Example_3;
+
+public sealed class ReportService(ILogger logger)
+{
+	private readonly ILogger _logger = logger;
+
+	public void Run() => _logger.Log("Report executed.");
+}
