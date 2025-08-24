3f3ef68fe846c637e4ca61f1f96a9752e7b29dbf
Kris <kris_s2@outlook.com>
2025-08-24 11:17:18 -0700

    kata: moving xmlLogger adapter.

--- src/ConAppPlayingWithAdapterPattern/Example_3/ILogger.cs
+++ src/ConAppPlayingWithAdapterPattern/Example_3/ILogger.cs
@@ -1,18 +1,7 @@
-﻿using System.Security;
-
-namespace ConAppPlayingWithAdapterPattern.Example_3;
+﻿namespace ConAppPlayingWithAdapterPattern.Example_3;
 public interface ILogger 
 { 
 	void Log(string message); 
-}
-
-public sealed class XmlLoggerAdapter() : ILogger
-{
-	public void Log(string message)
-	{
-		var xml = $"<log><timestamp>{DateTime.UtcNow:o}</timestamp><msg>{SecurityElement.Escape(message)}</msg></log>";
-		XmlAuditLogger.WriteXml(xml);
-	}
 }
 
 public sealed class ReportService(ILogger logger)
---
+++ src/ConAppPlayingWithAdapterPattern/Example_3/XmlLoggerAdapter.cs
@@ -0,0 +1,12 @@
+﻿using System.Security;
+
+namespace ConAppPlayingWithAdapterPattern.Example_3;
+
+public sealed class XmlLoggerAdapter() : ILogger
+{
+	public void Log(string message)
+	{
+		var xml = $"<log><timestamp>{DateTime.UtcNow:o}</timestamp><msg>{SecurityElement.Escape(message)}</msg></log>";
+		XmlAuditLogger.WriteXml(xml);
+	}
+}
