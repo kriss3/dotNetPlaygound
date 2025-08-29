7769de00155830c25256e1a45c439ce9bcbcde78
Kris <kris_s2@outlook.com>
2025-08-28 21:28:43 -0700

    kata: attempt to run Execute fnct();

--- src/ConAppCommandPattern/Program.cs
+++ src/ConAppCommandPattern/Program.cs
@@ -8,25 +8,23 @@ public class Program
     static void Main()
     {
         WriteLine("Executing Command Pattern!");
-
-     
+        var result = Execute();
     }
 
-    private string Run() 
+    private static string Execute() 
     {
-        await Task.Run(() =>
-        {
-            //this is my driver function. 
-            //I need: Receiver, Command, Invoker
-            Receiver receiver = new();
-            Command command = new ConcreteCommand(receiver);
-            Invoker invoker = new();
+        var result = "Success";
+        //this is my driver function. 
+        //I need: Receiver, Command, Invoker
+        Receiver receiver = new();
+        Command command = new ConcreteCommand(receiver);
+        Invoker invoker = new();
 
-            // now that I have base infrastructure ready make a setup:
-            invoker.SetCommand(command);
-            invoker.ExecuteCommand();
+        // now that I have base infrastructure ready make a setup:
+        invoker.SetCommand(command);
+        invoker.ExecuteCommand();
 
-            ReadKey();
-        });
+        ReadKey();
+        return result;
     }
 }
