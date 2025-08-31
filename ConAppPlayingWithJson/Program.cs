7498d6368fb0cdf792aa9c54da05fa6ead827ca1
Kris <kris_s2@outlook.com>
2025-08-31 14:58:16 -0700

    kata: minor cleanup.

--- ConAppPlayingWithJson/Program.cs
+++ ConAppPlayingWithJson/Program.cs
@@ -3,6 +3,10 @@
 
 namespace ConAppPlayingWithJson;
 
+/// <summary>
+/// Work with Models project and this to implement Result<TSuccess, TFailure> APIs
+/// And Option<T> examples using STJ.
+/// </summary>
 public class Program
 {
 	static void Main()
