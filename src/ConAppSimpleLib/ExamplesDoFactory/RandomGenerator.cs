36022ae7a000ccc05bf4d5a9b541ce8c581b2e4a
Kris <kris_s2@outlook.com>
2025-09-03 21:36:15 -0700

    kata: working with random and different variation for learning purposes.

--- src/ConAppSimpleLib/ExamplesDoFactory/RandomGenerator.cs
+++ src/ConAppSimpleLib/ExamplesDoFactory/RandomGenerator.cs
@@ -5,6 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConAppSimpleLib.ExamplesDoFactory;
-internal class RandomGenerator
+public class RandomGenerator
 {
+
+	public enum RandomNumberType 
+	{
+		month, dice, card
+	}
+
+	public static int GenerateNumber(RandomNumberType numberType) 
+	{
+
+		Random random = new();
+
+
+
+		// number between 1 .. 12
+		var month = random.Next(1, 13);
+
+		// number between 1 .. 6
+		var dice = random.Next(1, 7);
+
+		// number between 0 .. 51
+		var card = random.Next(52);
+
+		return 0;
+	}
+
 }
