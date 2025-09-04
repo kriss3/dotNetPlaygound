2f9baa10df9961e7f44650280f834b4481550399
Kris <kris_s2@outlook.com>
2025-09-03 21:48:39 -0700

    kata: fixing weird class formatting issue.

--- src/ConAppSimpleLib/ExamplesDoFactory/RandomGenerator.cs
+++ src/ConAppSimpleLib/ExamplesDoFactory/RandomGenerator.cs
@@ -5,41 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConAppSimpleLib.ExamplesDoFactory;
-public class RandomGenerator
-{
 
-	public enum RandomNumberType 
+public class RandomGenerator 
+{
+	public enum RandomNumberType
 	{
 		month, dice, card
 	}
 
-	public static int GenerateNumber(RandomNumberType numberType) 
+	public static int GenerateNumber(RandomNumberType numberType)
 	{
-
 		Random random = new();
 
-		var res = switch
-		{
-			// number between 1 .. 12
-			numberType.month => random.Next(1, 13), 
-			// number between 1 .. 6
-			numberType.dice => random.Next(1, 7),
-			numberType.card => random.Next(0, 52)
-			_ => throw new ArgumentOutOfRangeException(nameof(numberType), "Unsupported number Type.");
-			Rand
-		};
+		var result = 0;
 
-
-		
-		var month = random.Next(1, 13);
-
-		
-		var dice = random.Next(1, 7);
-
-		// number between 0 .. 51
-		var card = random.Next(52);
-
-		return 0;
+		return result;
 	}
-
 }
