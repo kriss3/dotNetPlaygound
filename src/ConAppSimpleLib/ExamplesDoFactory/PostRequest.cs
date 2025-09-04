360ae67b01fff73f4786ace7f7e1114ec0e14275
Kris <kris_s2@outlook.com>
2025-09-03 21:03:46 -0700

    kata: adding values/content/response/result

--- src/ConAppSimpleLib/ExamplesDoFactory/PostRequest.cs
+++ src/ConAppSimpleLib/ExamplesDoFactory/PostRequest.cs
@@ -7,6 +7,16 @@ public class PostRequest
 
 	public async Task PostViaHttp() 
 	{
-		
+		var values = new Dictionary<string, string>
+		{
+		   { "thing1", "hello" },
+		   { "thing2", "world" }
+		};
+
+		var content = new FormUrlEncodedContent(values);
+
+		var response = await client.PostAsync("http://www.example.com/postable.aspx", content);
+
+		var result = await response.Content.ReadAsStringAsync();
 	}
 }
