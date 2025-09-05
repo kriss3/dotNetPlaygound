e85371f4dedc96e2ec3d1ad5388bd0bc08153891
Kris <kris_s2@outlook.com>
2025-09-04 21:34:01 -0700

    kata: begining of working with json serializer. Simple approach.

--- src/ConAppSimpleLib/ExamplesDoFactory/MySerializer.cs
+++ src/ConAppSimpleLib/ExamplesDoFactory/MySerializer.cs
@@ -5,6 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConAppSimpleLib.ExamplesDoFactory;
-internal class MySerializer
+public class MySerializer
 {
+	public int DoSerializae() 
+	{
+		var result = -1;
+
+		var obj = new Person
+		{
+			FirstName = "Sander",
+			LastName = "Chaney",
+			Email = "schaney@gmail.com",
+			DateOfBirth = new MyDate
+			{
+				Year = 1988,
+				Month = 4,
+				Day = 30
+			}
+		};
+
+		return result;
+	}
 }
