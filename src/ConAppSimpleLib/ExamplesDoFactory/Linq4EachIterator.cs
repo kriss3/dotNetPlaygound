a1225ca1c9c32086b61f66842a16aff9492b7a0d
Kris <kris_s2@outlook.com>
2025-09-08 22:24:52 -0700

    kata: adding first exemple.

--- src/ConAppSimpleLib/ExamplesDoFactory/Linq4EachIterator.cs
+++ src/ConAppSimpleLib/ExamplesDoFactory/Linq4EachIterator.cs
@@ -3,8 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ConAppSimpleLib.ExamplesDoFactory;
-internal class Linq4EachIterator
+public class Linq4EachIterator
 {
+	public static void DriveUsingSimple4Each() 
+	{
+		var numbers = GetNumbers();
+		var index = 0;
+
+		foreach (var item in numbers)
+		{
+			Console.WriteLine($"Index: {index}. Value: {item}");
+			index++;
+		}
+	}
+
+
+	private static IEnumerable<int> GetNumbers()
+	{
+		return [1, 2, 3, 4, 8, 10];
+	}
 }
