313fdc4d6ad6677e4368832ec675ab968039ea49
Kris <kris_s2@outlook.com>
2025-09-09 23:40:17 -0700

    kata: cleanup

--- src/ConAppSimpleLib/ExamplesDoFactory/OptionDemo.cs
+++ src/ConAppSimpleLib/ExamplesDoFactory/OptionDemo.cs
@@ -1,9 +1,4 @@
 ﻿using Cova.Functional;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConAppSimpleLib.ExamplesDoFactory;
 public static class OptionDemo
