7e0009b6ddb4f20049e79ce910107f7dd7e68926
Kris <kris_s2@outlook.com>
2025-09-09 23:32:24 -0700

    kata: adding the second fnct.

--- src/ConAppSimpleLib/ExamplesDoFactory/OptionDemo.cs
+++ src/ConAppSimpleLib/ExamplesDoFactory/OptionDemo.cs
@@ -8,8 +8,14 @@ using System.Threading.Tasks;
 namespace ConAppSimpleLib.ExamplesDoFactory;
 public static class OptionDemo
 {
-
+	// Returns Some(n) when input is a positive int; otherwise None<int>()
 	public static Option<int> TryParsePositiveInt(string input) 
+	{
+		throw new NotImplementedException();
+	}
+
+	// Example of getting an environment variable as Option<string>
+	public static Option<string> TryGetEnv(string variable) 
 	{
 		throw new NotImplementedException();
 	}
