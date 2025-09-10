9ec6cce32ac6030b51340da1e5de9abe69091d5c
Kris <kris_s2@outlook.com>
2025-09-09 23:31:11 -0700

    kata: adding functon frames.

--- src/ConAppSimpleLib/ExamplesDoFactory/OptionDemo.cs
+++ src/ConAppSimpleLib/ExamplesDoFactory/OptionDemo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cova.Functional;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,4 +8,9 @@ using System.Threading.Tasks;
 namespace ConAppSimpleLib.ExamplesDoFactory;
 public static class OptionDemo
 {
+
+	public static Option<int> TryParsePositiveInt(string input) 
+	{
+		throw new NotImplementedException();
+	}
 }
