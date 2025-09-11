85b78b14437ad134e5209a53c5c6a8a7197d3b0e
Kris <kris_s2@outlook.com>
2025-09-10 22:35:16 -0700

    kata: adding implementions

--- src/ConAppSimpleLib/ExamplesDoFactory/CollectionService.cs
+++ src/ConAppSimpleLib/ExamplesDoFactory/CollectionService.cs
@@ -8,6 +8,10 @@ namespace ConAppSimpleLib.ExamplesDoFactory;
 public class CollectionService
 {
 	// Combine two collections using collection expressions
+	public IEnumerable<int> CombineCollections(List<int> firstList, List<int> secondCollection) =>
+		[.. firstList, .. secondCollection];
 
 	//Filter a collection using LINQ and return a collection expression
+	public IEnumerable<int> GetEvenNumber(List<int> numbers) =>
+		[.. numbers.Where(n => n % 2 == 0)];
 }
