5fb2f8c04f2ead14f0cabd75de4bf83eb7065177
Kris <kris_s2@outlook.com>
2025-09-11 21:14:19 -0700

    kata: adding two more concepts to explore.

--- src/ConAppSimpleLib/ExamplesDoFactory/CollectionService.cs
+++ src/ConAppSimpleLib/ExamplesDoFactory/CollectionService.cs
@@ -1,11 +1,14 @@
 ﻿namespace ConAppSimpleLib.ExamplesDoFactory;
 public class CollectionService
 {
-	// Combine two collections using collection expressions
+	// 1: Combine two collections using collection expressions
 	public IEnumerable<int> CombineCollections(List<int> firstList, List<int> secondCollection) =>
 		[.. firstList, .. secondCollection];
 
-	//Filter a collection using LINQ and return a collection expression
+	// 2: Filter a collection using LINQ and return a collection expression
 	public IEnumerable<int> GetEvenNumbers(List<int> numbers) =>
 		[.. numbers.Where(n => n % 2 == 0)];
+
+	// 3: Reverse numbers using Span<T>
+	// 4: Get a substring using ReadOnlySpan<T>
 }
