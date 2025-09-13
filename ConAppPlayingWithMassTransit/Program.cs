3bac732ba14d9f61e8e9e333ed2dfc8ae0dc5cb4
Kris <kris_s2@outlook.com>
2025-09-12 23:23:12 -0700

    kata: more preparations.

--- ConAppPlayingWithMassTransit/Program.cs
+++ ConAppPlayingWithMassTransit/Program.cs
@@ -2,10 +2,16 @@
 
 namespace ConAppPlayingWithMassTransit;
 
-internal class Program
+public class Program
 {
 	static void Main(string[] args)
 	{
 		WriteLine("This is MassTransit - library for abstracting exchanging messages between systems.");
 	}
 }
+
+public class PublisherService(IBus bus, ILogger<PublisherService> log) : BackgroundService
+{ }
+
+public class HelloConsumer(ILogger<HelloConsumer> log) : IConsumer<Hello>
+{ }
