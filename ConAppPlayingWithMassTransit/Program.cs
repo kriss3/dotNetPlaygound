38326d52ee728cd33397df44d911a2b1c055e409
Kris <kris_s2@outlook.com>
2025-09-14 17:46:27 -0700

    kata: cleanup

--- ConAppPlayingWithMassTransit/Program.cs
+++ ConAppPlayingWithMassTransit/Program.cs
@@ -3,17 +3,11 @@ using MassTransit;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using static MassTransit.MessageHeaders;
-using static MassTransit.Monitoring.Performance.BuiltInCounters;
-using static MassTransit.Util.ChartTable;
-
+using static System.Console;
 //Adding Aliases:
 using MsHost = Microsoft.Extensions.Hosting.Host;
 using MsIHost = Microsoft.Extensions.Hosting.IHost;
 using MsIHostBuilder = Microsoft.Extensions.Hosting.IHostBuilder;
-
-
-using static System.Console;
 
 
 namespace ConAppPlayingWithMassTransit;
@@ -23,7 +17,9 @@ public class Program
 	public static async Task Main(string[] args)
 	{
 		WriteLine("This is MassTransit - library for abstracting exchanging messages between systems.");
-		var res = CreateHostBuilder();
+		MsIHost host = CreateHostBuilder()
+			.Build();
+		await host.RunAsync();
 	}
 
 	private static MsIHostBuilder CreateHostBuilder() 
@@ -56,7 +52,6 @@ public class PublisherService(IBus bus, ILogger<PublisherService> log) : Backgro
 	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
 	{
 		// Publish a few messages, then keep the host alive
-
 		for (var i = 1; i <= 5 && !stoppingToken.IsCancellationRequested; i++)
 		{
 			var name = $"Kris #{i}";
