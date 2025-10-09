6a87c6ccf91e9a49becab7ef115aab1059ca158e
Kris <kris_s2@outlook.com>
2025-10-08 22:50:29 -0700

    kata: minor fixes.

--- src/TestingUnitTests/CalculatorTests.cs
+++ src/TestingUnitTests/CalculatorTests.cs
@@ -1,52 +1,52 @@
 ﻿using Xunit;
-namespace TestingUnitTests
+
+namespace TestingUnitTests;
+
+public class CalculatorTests
 {
-    public class CalculatorTests
+    [Fact]
+    public void Add_AddsCorrectlyTwoNumber_ReturnsNumber() 
     {
-        [Fact]
-        public void Add_AddsCorrectlyTwoNumber_ReturnsNumber() 
-        {
-            //Arrange
-            double expected = 20;
-            //Act
-            var actual = Calculator.Add(10, 10);
-            //Assert
-            Assert.Equal(expected, actual);
-
-        }
-        [Fact]
-        public void Subtrac_SubtractCorrectlyTwoNumber_ReturnsNumber()
-        {
-            //Arrange
-            double expected = 20;
-            //Act
-            var actual = Calculator.Subtact(30, 10);
-            //Assert
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void Multiply_MultiplyCorrectlyTwoNumber_ReturnsNumber()
-        {
-            //Arrange
-            double expected = 300;
-            //Act
-            var actual = Calculator.Multiply(30, 10);
-            //Assert
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void Divide_DivideCorrectlyTwoNumber_ReturnsNumber()
-        {
-            //Arrange
-            double expected = 10;
-            //Act
-            var actual = Calculator.Divide(100, 10);
-            //Assert
-            Assert.Equal(expected, actual);
-        }
-
+        //Arrange
+        double expected = 20;
+        //Act
+        var actual = Calculator.Add(10, 10);
+        //Assert
+        Assert.Equal(expected, actual);
 
     }
+    [Fact]
+    public void Subtrac_SubtractCorrectlyTwoNumber_ReturnsNumber()
+    {
+        //Arrange
+        double expected = 20;
+        //Act
+        var actual = Calculator.Subtact(30, 10);
+        //Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Multiply_MultiplyCorrectlyTwoNumber_ReturnsNumber()
+    {
+        //Arrange
+        double expected = 300;
+        //Act
+        var actual = Calculator.Multiply(30, 10);
+        //Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Divide_DivideCorrectlyTwoNumber_ReturnsNumber()
+    {
+        //Arrange
+        double expected = 10;
+        //Act
+        var actual = Calculator.Divide(100, 10);
+        //Assert
+        Assert.Equal(expected, actual);
+    }
+
+
 }
