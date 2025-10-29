799b7b430c943da447cb24f892a1c3259e6f17c8
Kris <kris_s2@outlook.com>
2025-10-28 22:35:10 -0700

    kata: adding the basic setup.

--- src/ConAppPlayingWithFacadePattern/Program.cs
+++ src/ConAppPlayingWithFacadePattern/Program.cs
@@ -1,3 +1,12 @@
-﻿
+﻿using System.Diagnostics;
+using static System.Console;
 
+namespace ConAppPlayingWithFacadePattern;
 
+class Program
+{
+	public static void Main()
+	{
+		WriteLine($"Start the work on Facade Pattern.");
+	}
+}
