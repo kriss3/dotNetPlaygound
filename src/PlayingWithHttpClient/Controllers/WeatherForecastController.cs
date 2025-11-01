e180bb15435356272acb882d42c557e2691444a8
Kris <kris_s2@outlook.com>
2025-11-01 11:08:59 -0700

    kata: cleanup and using modern features of C#

--- src/PlayingWithHttpClient/Controllers/WeatherForecastController.cs
+++ src/PlayingWithHttpClient/Controllers/WeatherForecastController.cs
@@ -3,17 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 namespace PlayingWithHttpClient.Controllers;
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class WeatherForecastController(ILogger<WeatherForecastController> logger) : ControllerBase
 {
 	private static readonly string[] Summaries =
 		["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
 
-	private readonly ILogger<WeatherForecastController> _logger;
-
-	public WeatherForecastController(ILogger<WeatherForecastController> logger)
-	{
-		_logger = logger;
-	}
+	private readonly ILogger<WeatherForecastController> _logger = logger;
 
 	[HttpGet(Name = "GetWeatherForecast")]
 	public IEnumerable<WeatherForecast> Get()
