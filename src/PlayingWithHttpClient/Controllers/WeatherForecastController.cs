e6c9b0c7c3b4afb55676b0cdcae9e3aa8b718d85
Kris <kris_s2@outlook.com>
2025-11-01 11:07:02 -0700

    kata: fixing coll init in the new project.

--- src/PlayingWithHttpClient/Controllers/WeatherForecastController.cs
+++ src/PlayingWithHttpClient/Controllers/WeatherForecastController.cs
@@ -5,10 +5,8 @@ namespace PlayingWithHttpClient.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-	private static readonly string[] Summaries = new[]
-	{
-		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-	};
+	private static readonly string[] Summaries =
+		["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
 
 	private readonly ILogger<WeatherForecastController> _logger;
 
