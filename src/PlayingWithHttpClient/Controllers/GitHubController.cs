b4870b0d934cf867d91756819dbd6ec383a3aadc
Kris <kris_s2@outlook.com>
2025-11-06 22:51:02 -0800

    kata: adding GitHubSettings model

--- src/PlayingWithHttpClient/Controllers/GitHubController.cs
+++ src/PlayingWithHttpClient/Controllers/GitHubController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PlayingWithHttpClient.Models;
 
 namespace PlayingWithHttpClient.Controllers;
 [Route("api/[controller]")]
 [ApiController]
 public class GitHubController : ControllerBase
 {
+	private readonly HttpClient _httpClient;
+	private readonly GitHubSettings _settings;
+
+	public GitHubController()
+	{
+		
+	}
+
 }
---
+++ src/PlayingWithHttpClient/Models/GitHubSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlayingWithHttpClient.Models;
+
+public sealed class GitHubSettings
+{
+	public const string ConfigurationSection = "GitHub";
+
+	[Required]
+	public string AccessToken { get; init; } = string.Empty;
+
+	[Required]
+	public string UserAgent { get; init; } = string.Empty;
+}
