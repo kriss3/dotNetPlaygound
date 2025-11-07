75697c0d50f80a0db29977c579f7c9882a79c070
Kris <kris_s2@outlook.com>
2025-11-06 22:40:47 -0800

    kata: for this purpose of this excercise, removing logging injection.

--- src/PlayingWithHttpClient/Controllers/WeatherForecastController.cs
+++ src/PlayingWithHttpClient/Controllers/WeatherForecastController.cs
@@ -4,12 +4,11 @@ using PlayingWithHttpClient.Models;
 namespace PlayingWithHttpClient.Controllers;
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController(ILogger<WeatherForecastController> logger) : ControllerBase
+public class WeatherForecastController : ControllerBase
 {
 	private static readonly string[] Summaries =
 		["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
 
-	private readonly ILogger<WeatherForecastController> _logger = logger;
 
 	[HttpGet(Name = "GetWeatherForecast")]
 	public IEnumerable<WeatherForecast> Get()
