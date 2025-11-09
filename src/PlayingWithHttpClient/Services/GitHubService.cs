fea890928302cc113e79cd09eb2ac72cd0f86451
Kris <kris_s2@outlook.com>
2025-11-08 22:13:15 -0800

    kata: adding client to the service.

--- src/PlayingWithHttpClient/Services/GitHubService.cs
+++ src/PlayingWithHttpClient/Services/GitHubService.cs
@@ -1,4 +1,12 @@
 ﻿namespace PlayingWithHttpClient.Services;
 
 public sealed class GitHubService
-{ }
+{
+	private readonly HttpClient _client;
+
+	public GitHubService(HttpClient client)
+	{
+		_client = client;
+	}
+
+}
