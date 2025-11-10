3bb30cca3085eac0be36f108867a66aab96a3682
Kris <kris_s2@outlook.com>
2025-11-09 18:38:09 -0800

    kata: adding configuration of the typed httpclient which is registered as Transient servicer. But when used from within Singleton (typed GitHubSerivce) it requires extra setup.

--- src/PlayingWithHttpClient/Program.cs
+++ src/PlayingWithHttpClient/Program.cs
@@ -26,14 +26,21 @@ public class Program
 		});
 
 		// Typed HttpClient, HttpClient tight to the GitHubService.
-		builder.Services.AddHttpClient<GitHubService>((serviceProvider, httpClient) => 
+		// REMEMBER: This is Transient service registreation with DI.
+		// If using Typed client make sure to configure PrimaryMessageHandler when u want to use Transient service from Singleton.
+		builder.Services.AddHttpClient<GitHubService>((serviceProvider, httpClient) =>
 		{
 			var gitHubSettings = serviceProvider.GetRequiredService<IOptions<GitHubSettings>>().Value;
 
 			httpClient.DefaultRequestHeaders.Add("Authorization", gitHubSettings.AccessToken);
 			httpClient.DefaultRequestHeaders.Add("User-Agent", gitHubSettings.UserAgent);
 			httpClient.BaseAddress = new Uri("https://api.github.com");
-		});
+		})
+		.ConfigurePrimaryHttpMessageHandler(() => 
+		{
+			return new SocketsHttpHandler { PooledConnectionIdleTimeout = TimeSpan.FromMinutes(5) };
+		})
+		.SetHandlerLifetime(Timeout.InfiniteTimeSpan);
 
 		builder.Services.AddControllers();
 		builder.Services.AddOpenApi();
