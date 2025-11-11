2f9364336d557d77db426c48960c3c4bd5919201
Kris <kris_s2@outlook.com>
2025-11-10 22:34:01 -0800

    kata: adding class AdvancedDelegates.

---
+++ src/ConAppDelegates/v3/AdvancedDelegates.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppDelegates.v3;
+public class AdvancedDelegates
+{
+}
