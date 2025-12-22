7461177c16b0d5ca4a42af5c7b1f23e44ca67a7e
Kris <kris_s2@outlook.com>
2025-12-21 17:37:59 -0800

    kata: adding Monkey inmplementation.

--- ConAppPlayingWithSqlChangeTracker/Models/Monkey.cs
+++ ConAppPlayingWithSqlChangeTracker/Models/Monkey.cs
@@ -1,9 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ConAppPlayingWithSqlChangeTracker.Models;
+﻿namespace ConAppPlayingWithSqlChangeTracker.Models;
 
 public class Monkey
 {
+	public int MonkeyId { get; set; }
+	public string? Name { get; set; }
+	public string? Location { get; set; }
+	public string? Details { get; set; }
+	public string? Image { get; set; }
+	public int Population { get; set; }
+	public decimal Latitude { get; set; }
+	public decimal Longitude { get; set; }
+	public DateTime CreatedDate { get; set; }
 }
