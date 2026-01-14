fc8ccd536d487c870a9e9e349275afe93303ffdc
Kris <kris_s2@outlook.com>
2026-01-13 20:59:48 -0800

    kata: adding new project to play with GTFS of BC Translink.

---
+++ PlayingWithTranslink.Gtfs/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PlayingWithTranslink.Gtfs;
+
+using static System.Console;
+
+public class Program
+{
+    static void Main()
+    {
+        WriteLine("Playing with BC Translink GTFS system API.");
+    }
+}
