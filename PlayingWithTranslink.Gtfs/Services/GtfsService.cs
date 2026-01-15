184378a07119bd7475f1452f082fb939d64cedeb
Kris <kris_s2@outlook.com>
2026-01-14 20:21:32 -0800

    kata: adding scaffolding of the service.

--- PlayingWithTranslink.Gtfs/Services/GtfsService.cs
+++ PlayingWithTranslink.Gtfs/Services/GtfsService.cs
@@ -1,15 +1,23 @@
-﻿using System;
+﻿using PlayingWithTranslink.Gtfs.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PlayingWithTranslink.Gtfs.Services;
 
-public sealed class GtfsService
+public sealed class GtfsService(string gtfsFolder)
 {
-	private readonly string _gtfsFolder;
+	private readonly string _gtfsFolder = gtfsFolder ?? throw new ArgumentNullException(nameof(gtfsFolder));
 
-	public GtfsService(string gtfsFolder)
+	public IReadOnlyList<Stop> SearchStops(string query, int take = 20)
 	{
-		_gtfsFolder = gtfsFolder ?? throw new ArgumentNullException(nameof(gtfsFolder));
+	}
+
+	public IReadOnlyList<string> GetNextScheduledDepartures(string stopId, TimeSpan now, int take = 10)
+	{
+	}
+
+	public IReadOnlyList<string> GetNextDeparturesWithRoute(string stopId, TimeSpan now, int take = 10)
+	{
 	}
 }
