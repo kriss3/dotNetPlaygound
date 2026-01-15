857f28b16c1eacac06b67aa11bd364b21578e8b2
Kris <kris_s2@outlook.com>
2026-01-14 20:15:30 -0800

    kata: adding parse GTFS time to return hours, minues, seconds.

--- PlayingWithTranslink.Gtfs/Infrastructure/GtfsTime.cs
+++ PlayingWithTranslink.Gtfs/Infrastructure/GtfsTime.cs
@@ -1,13 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PlayingWithTranslink.Gtfs.Infrastructure;
+﻿namespace PlayingWithTranslink.Gtfs.Infrastructure;
 
 public static class GtfsTime
 {
 	public static TimeSpan? ParseGtfsTime(string hhmmss)
-	{ 
-	
+	{
+		var parts = hhmmss.Split(':');
+		if (parts.Length != 3) return null;
+
+		if (!int.TryParse(parts[0], out var h)) return null;
+		if (!int.TryParse(parts[1], out var m)) return null;
+		if (!int.TryParse(parts[2], out var s)) return null;
+
+		if (m is < 0 or > 59) return null;
+		if (s is < 0 or > 59) return null;
+		if (h < 0) return null;
+
+		return new TimeSpan(h, m, s);
 	}
 }
