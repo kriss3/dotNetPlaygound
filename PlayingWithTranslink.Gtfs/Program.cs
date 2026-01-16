519d5397bcad1d713e173a13efffaf7a16ffad42
Kris <kris_s2@outlook.com>
2026-01-15 21:42:27 -0800

    kata: working on orchestration: putting Models, services and infra all together.

--- PlayingWithTranslink.Gtfs/Program.cs
+++ PlayingWithTranslink.Gtfs/Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace PlayingWithTranslink.Gtfs;
 
+using PlayingWithTranslink.Gtfs.Services;
 using static System.Console;
 
 public class Program
@@ -7,5 +8,15 @@ public class Program
     static void Main()
     {
         WriteLine("Playing with BC Translink GTFS system API.");
-    }
+		var gtfsFolder = Path.Combine(AppContext.BaseDirectory, "Gtfs");
+
+		if (!Directory.Exists(gtfsFolder))
+		{
+			WriteLine($"Missing folder: {gtfsFolder}");
+			WriteLine("Make sure Gtfs/*.txt are set to Copy if newer.");
+			return;
+		}
+
+		var svc = new GtfsService(gtfsFolder);
+	}
 }
