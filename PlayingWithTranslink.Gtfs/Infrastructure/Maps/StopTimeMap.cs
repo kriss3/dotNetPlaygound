6e835f3efd2f542133d333b5ad7a542ed0165285
Kris <kris_s2@outlook.com>
2026-01-16 22:40:48 -0800

    kata: adding implementation of StopTime map.

--- PlayingWithTranslink.Gtfs/Infrastructure/Maps/StopTimeMap.cs
+++ PlayingWithTranslink.Gtfs/Infrastructure/Maps/StopTimeMap.cs
@@ -1,9 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using CsvHelper.Configuration;
+using PlayingWithTranslink.Gtfs.Models;
 
 namespace PlayingWithTranslink.Gtfs.Infrastructure.Maps;
 
-internal class StopTimeMap
+public sealed class StopTimeMap : ClassMap<StopTime>
 {
+    public StopTimeMap()
+    {
+		Map(m => m.TripId).Name("trip_id");
+		Map(m => m.ArrivalTime).Name("arrival_time");
+		Map(m => m.DepartureTime).Name("departure_time");
+		Map(m => m.StopId).Name("stop_id");
+		Map(m => m.StopSequence).Name("stop_sequence");
+	}
 }
