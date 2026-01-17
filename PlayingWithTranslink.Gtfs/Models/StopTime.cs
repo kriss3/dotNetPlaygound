1f361ee8f73f78c77c9fd948d9590f3d745b992c
Kris <kris_s2@outlook.com>
2026-01-16 22:29:43 -0800

    kata: updating StopTime model

--- PlayingWithTranslink.Gtfs/Models/StopTime.cs
+++ PlayingWithTranslink.Gtfs/Models/StopTime.cs
@@ -1,3 +1,3 @@
 ﻿namespace PlayingWithTranslink.Gtfs.Models;
 
-public sealed record StopTime(string trip_id, string arrival_time, string departure_time, string stop_id, int stop_sequence);
+public sealed record StopTime(string TripId, string ArrivalTime, string DepartureTime, string StopId, int StopSequence);
