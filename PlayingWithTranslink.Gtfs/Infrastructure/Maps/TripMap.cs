45942acc5b9a70071b9e33af62f2d13899947ca9
Kris <kris_s2@outlook.com>
2026-01-16 22:38:55 -0800

    kata: adding implementation of TripMap.

--- PlayingWithTranslink.Gtfs/Infrastructure/Maps/TripMap.cs
+++ PlayingWithTranslink.Gtfs/Infrastructure/Maps/TripMap.cs
@@ -1,9 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using CsvHelper.Configuration;
+using PlayingWithTranslink.Gtfs.Models;
 
 namespace PlayingWithTranslink.Gtfs.Infrastructure.Maps;
 
-internal class TripMap
+public sealed class TripMap : ClassMap<Trip>
 {
+    public TripMap()
+    {
+		Map(m => m.RouteId).Name("route_id");
+		Map(m => m.ServiceId).Name("service_id");
+		Map(m => m.TripId).Name("trip_id");
+		Map(m => m.TripHeadSign).Name("trip_headsign");
+	}
 }
