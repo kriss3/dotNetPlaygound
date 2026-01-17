29edac902122cafbe3282098c83f5909bb0b8457
Kris <kris_s2@outlook.com>
2026-01-16 22:28:31 -0800

    kata: updating Stop model

--- PlayingWithTranslink.Gtfs/Models/Stop.cs
+++ PlayingWithTranslink.Gtfs/Models/Stop.cs
@@ -1,8 +1,3 @@
 ﻿namespace PlayingWithTranslink.Gtfs.Models;
 
-public sealed record Stop(
-	string StopId, 
-	string StopName, 
-	double StopLatitude, 
-	double StopLongitute);
-
+public sealed record Stop(string StopId, string StopName, double StopLatitude, double StopLongitute);
