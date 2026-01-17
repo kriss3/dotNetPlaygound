53587cbc6c93d896a1d093cfbe2b7361ce6a8e1c
Kris <kris_s2@outlook.com>
2026-01-16 22:37:39 -0800

    kata: adding implementation of StopMap

--- PlayingWithTranslink.Gtfs/Infrastructure/Maps/StopMap.cs
+++ PlayingWithTranslink.Gtfs/Infrastructure/Maps/StopMap.cs
@@ -1,9 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using CsvHelper.Configuration;
+using PlayingWithTranslink.Gtfs.Models;
 
 namespace PlayingWithTranslink.Gtfs.Infrastructure.Maps;
 
-internal class StopMap
+public sealed class StopMap: ClassMap<Stop>
 {
+    public StopMap()
+    {
+        Map(m => m.StopId).Name("stop_id");
+		Map(m => m.StopName).Name("stop_name");
+		Map(m => m.StopLatitude).Name("stop_lat");
+		Map(m => m.StopLongitute).Name("stop_lon");
+	}
 }
